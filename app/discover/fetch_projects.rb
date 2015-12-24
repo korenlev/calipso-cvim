bc8860b20dedd69018e09b5cce995c645afdfad4
yaronyogev <yaronyogev@gmail.com>
2015-12-24 17:35:30 +0200

    fetch project description as well

--- app/discover/fetch_projects.rb
+++ app/discover/fetch_projects.rb
@@ -4,7 +4,7 @@ require_relative 'db_access'
 class FetchProjects < DbAccess
   
   def get_projects()
-    return get_objects("SELECT id, name FROM keystone.project WHERE enabled")
+    return get_objects("SELECT id, name, description FROM keystone.project WHERE enabled")
   end
   
 end
