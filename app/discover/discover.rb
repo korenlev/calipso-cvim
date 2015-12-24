539a8f3ca120af9004468b81852ec4537c5ad85e
Yaron Yogev <yayogev@cisco.com>
2015-12-24 21:35:52 +0200

    changed to work in CGI mode

--- app/discover/discover.rb
+++ app/discover/discover.rb
@@ -1,14 +1,24 @@
+#!/usr/bin/ruby
+
+require 'cgi'
 require_relative 'fetch_regions'
 require_relative 'fetch_projects'
 
-region_fetcher = FetchRegions.new()
-region_fetcher.set_prettify(true)
-region_fetcher.connect("10.56.20.74", 13306, "root", "BsfYNGxi", "nova")
-print "Regions:\n"
-print region_fetcher.get_regions()
-print "\n"
-print "--------\n"
-print "Projects:\n"
-print "---------\n"
-project_fetcher = FetchProjects.new()
-print project_fetcher.get_projects()
+cgi = CGI.new
+what_to_fetch = cgi.params["type"][0]
+
+id = cgi.params["id"] || ""
+
+fetcher = nil
+case what_to_fetch
+  when "regions"
+    fetcher = FetchRegions.new()
+  when "projects"
+    fetcher = FetchProjects.new()
+  else
+    fetcher = DbAccess.new()
+end
+fetcher.set_prettify(true)
+fetcher.connect("10.56.20.74", 13306, "root", "BsfYNGxi", "nova")
+response = fetcher.get()
+cgi.out("application/json") { response }
