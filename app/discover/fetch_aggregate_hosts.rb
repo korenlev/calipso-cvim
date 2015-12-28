bef565cdcd8805ed1bb62b580cec7870e15f5ae7
Yaron Yogev <yayogev@cisco.com>
2015-12-28 19:29:48 +0200

    bug fix: did not quote id parameter in query, also formatted query

--- app/discover/fetch_aggregate_hosts.rb
+++ app/discover/fetch_aggregate_hosts.rb
@@ -4,7 +4,11 @@ require_relative 'db_access'
 class FetchAggregateHosts < DbAccess
   
   def get(id)
-    query = %Q{SELECT id, host AS name FROM nova.aggregate_hosts WHERE deleted = 0 AND aggregate_id = #{id}}
+    query = %Q{
+      SELECT id, host AS name
+      FROM nova.aggregate_hosts
+      WHERE deleted = 0 AND aggregate_id = '#{id}'
+    }
     return get_objects(query, "host")
   end
   
