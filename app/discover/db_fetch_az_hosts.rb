3f1bf725ea6ccd49e336d47f4c01082dda74ed9a
yaronyogev <yaronyogev@gmail.com>
2016-01-20 16:18:35 +0200

    ignore deleted hosts, or cases of null availability zone

--- app/discover/db_fetch_az_hosts.rb
+++ app/discover/db_fetch_az_hosts.rb
@@ -6,7 +6,10 @@ class DbFetchAZHosts < DbAccess
     query = %Q{
       SELECT DISTINCT host, host AS id
       FROM nova.instances
-      WHERE availability_zone = '#{id}' AND host IS NOT NULL
+      WHERE availability_zone = '#{id}'
+        AND host IS NOT NULL
+        AND availability_zone IS NOT NULL
+        AND deleted = 0
     }
     return get_objects_list(query, "host")
   end
