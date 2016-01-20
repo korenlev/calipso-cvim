bbaef4c19641d7ec87a373e770c256a3c42ad231
yaronyogev <yaronyogev@gmail.com>
2016-01-20 16:56:33 +0200

    fix handling of ID & type fetching from obj, handle different DB result formats

--- app/discover/scanner.rb
+++ app/discover/scanner.rb
@@ -21,8 +21,8 @@ class Scanner
   
   def scan(obj)
     obj && symbolize_keys_deep!(obj)
-    @id = obj ? obj[:id] : nil
-    if obj && (@id == nil || @id.to_s.rstrip.empty?)
+    @id = obj ? obj[:id].to_s : nil
+    if obj && (@id == nil || @id.rstrip.empty?)
       raise ArgumentError, "Object missing id attribute"
     end
     @types_to_fetch.each {|t|
@@ -34,13 +34,19 @@ class Scanner
     fetcher = type_to_fetch[:fetcher]
     children_scanner = type_to_fetch[:children_scanner]
     escaped_id = @id ? fetcher.escape(@id.to_s) : @id
-    objects = fetcher.get(escaped_id)
-    results = objects["rows"]
+    db_results = fetcher.get(escaped_id)
+    case
+    when db_results.class.name == "Hash"
+      results = db_results["rows"] || [db_results]
+    when db_results.class.name == "Array"
+      results = db_results
+    end
     results.each {|o|
       o[:environment] = @@environment
-      o[:type] = objects["type"]
+      o[:type] ||= o["type"]
+      o[:type] ||= db_results["type"]
       if parent
-        o[:parent_id] = parent[:id]
+        o[:parent_id] = parent[:id].to_s
         o[:parent_type] = parent[:type]
       end
       @@inventory.set(o)
