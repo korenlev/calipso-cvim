ad19940576fc761be1d7c9fb0f9ee7a9f134ab6d
Yaron Yogev <yayogev@cisco.com>
2016-03-16 15:53:56 +0200

    add path attributes: id_path, name_path

--- app/discover/scanner.py
+++ app/discover/scanner.py
@@ -52,9 +52,16 @@ class Scanner(Util):
       child_id_field = type_to_fetch["object_id_to_use_in_child"]
     except KeyError:
       child_id_field = "id"
+    environment = Scanner.environment
     for o in results:
-      o["environment"] = Scanner.environment
+      o["environment"] = environment
       o["type"] = type_to_fetch["type"] if type_to_fetch["type"] else o["type"]
+      try:
+        parent_id_path = parent["id_path"]
+        parent_name_path = parent["name_path"]
+      except KeyError:
+         parent_id_path = "/" + environment
+         parent_name_path = "/" + environment
       try:
          # case of dynamic folder added by need
          master_parent_type = o["master_parent_type"]
@@ -64,6 +71,8 @@ class Scanner(Util):
            "parent_id": master_parent_id,
            "parent_type": master_parent_type,
            "id": o["parent_id"],
+           "id_path": parent_id_path + "/" + o["parent_id"],
+           "name_path": parent_name_path + "/" + o["parent_text"],
            "name": o["parent_id"],
            "type": o["parent_type"],
            "text": o["parent_text"]
@@ -72,6 +81,16 @@ class Scanner(Util):
       except KeyError:
          pass
 
+      o["id_path"] = parent_id_path + "/" + str(o["id"]).strip()
+      try:
+        name = o["text"]
+      except KeyError:
+        try:
+          name = o["name"]
+        except KeyError:
+          name = o["id"]
+      o["name_path"] = parent_name_path + "/" + name
+      
       if "parent_id" not in o and parent:
         parent_id = str(parent["id"])
         o["parent_id"] = parent_id
