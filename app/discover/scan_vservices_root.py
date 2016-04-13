9180227c87d93d13b6b2d5c82f813bb0e403edfe
Yaron Yogev <yayogev@cisco.com>
2016-04-13 18:10:17 +0300

    US166 add host type to allow different treatment of compute nodes and network nodes

--- app/discover/scan_vservices_root.py
+++ app/discover/scan_vservices_root.py
@@ -1,18 +1,14 @@
 from singleton import Singleton
 from scanner import Scanner
 from db_fetch_host_network_agents import DbFetchHostNetworkAgents
-from cli_fetch_host_network_agents import CliFetchHostNetworkAgents
+from cli_fetch_host_vservices import CliFetchHostVservices
 
 class ScanVservicesRoot(Scanner, metaclass=Singleton):
-  
+
   def __init__(self):
     super(ScanVservicesRoot, self).__init__([
       {
         "type": "vservice",
-        "fetcher": DbFetchHostNetworkAgents()
-      },
-      {
-        "type": "vservice",
-        "fetcher": CliFetchHostNetworkAgents()
+        "fetcher": CliFetchHostVservices()
       }
     ])
