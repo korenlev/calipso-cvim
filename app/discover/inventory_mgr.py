6cdfd4d08693a6b93ae5d8e7392ba9c2d64edb06
Yaron Yogev <yayogev@cisco.com>
2016-05-08 15:58:30 +0300

    US375 encode dots in key names before saving items to MongoDB

--- app/discover/inventory_mgr.py
+++ app/discover/inventory_mgr.py
@@ -13,7 +13,7 @@ class InventoryMgr(MongoAccess, Util):
 
   # return single match
   def get_by_id(self, environment, item_id):
-    matches = self.inv.find({
+    matches = self.find({
       "environment": environment,
       "id": item_id
     })
@@ -26,13 +26,13 @@ class InventoryMgr(MongoAccess, Util):
 
   def get_by_field(self, environment, item_type, field_name, field_value):
     if field_value and (not isinstance(field_value, str) or field_value > ""):
-      matches = self.inv.find({
+      matches = self.find({
         "environment": environment,
         "type": item_type,
         field_name: field_value
       })
     else:
-      matches = self.inv.find({
+      matches = self.find({
         "environment": environment,
         "type": item_type
       })
@@ -50,12 +50,12 @@ class InventoryMgr(MongoAccess, Util):
   def get_children(self, environment, item_type, parent_id):
     matches = []
     if parent_id and parent_id > "" and item_type == None:
-      matches = self.inv.find({"environment": environment, "parent_id": parent_id})
+      matches = self.find({"environment": environment, "parent_id": parent_id})
     else:
       if parent_id and parent_id > "":
-        matches = self.inv.find({"environment": environment, "type": item_type, "parent_id": parent_id})
+        matches = self.find({"environment": environment, "type": item_type, "parent_id": parent_id})
       else:
-        matches = self.inv.find({"environment": environment, "type": item_type})
+        matches = self.find({"environment": environment, "type": item_type})
     ret = []
     for doc in matches:
       doc["_id"] = str(doc["_id"])
@@ -64,7 +64,7 @@ class InventoryMgr(MongoAccess, Util):
     return ret
   
   def getSingle(self, environment, item_type, item_id):
-    matches = self.inv.find({"environment": environment, "type": item_type, "id": item_id})
+    matches = self.find({"environment": environment, "type": item_type, "id": item_id})
     ret = []
     for doc in matches:
       doc["_id"] = str(doc["_id"])
@@ -93,7 +93,7 @@ class InventoryMgr(MongoAccess, Util):
     self.inv.update(
       {"environment": item["environment"],
        "type": item["type"], "id": item["id"]},
-      {'$set': item},
+      {'$set': self.encode_mongo_keys(item)},
       upsert=True)
     if projects:
       self.inv.update(
@@ -110,3 +110,9 @@ class InventoryMgr(MongoAccess, Util):
   def get_base_url(self, doc):
     return self.base_url_prefix + "&id=" + str(doc["id"])
 
+  def find(self, search):
+    matches = self.inv.find(search)
+    decoded_matches = []
+    for m in matches:
+      decoded_matches.append(self.decode_mongo_keys(m))
+    return decoded_matches
--- app/discover/mongo_access.py
+++ app/discover/mongo_access.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
 
+import re
+
 class MongoAccess:
   
   client = None
@@ -13,3 +15,39 @@ class MongoAccess:
       return
     MongoAccess.client = MongoClient('localhost', 27017)
     MongoAccess.db = MongoAccess.client.osdna
+
+  def encode_dots(self, s):
+    return s.replace(".", "[dot]")
+
+  def decode_dots(self, s):
+    return s.replace("[dot]", ".")
+
+  # Mongo will not accpet dot (".") in keys, or $ in start of keys
+  # $ in beginning of key does not happen in OpenStack,
+  # so need to translate only "." --> "[dot]"
+  def encode_mongo_keys(self, item):
+    return self.change_dict_naming_convention(item, self.encode_dots)
+
+  def decode_mongo_keys(self, item):
+    return self.change_dict_naming_convention(item, self.decode_dots)
+
+  # Convert a nested dictionary from one convention to another.
+  # Args:
+  #     d (dict): dictionary (nested or not) to be converted.
+  #     convert_function (func): function that takes the string in one convention and returns it in the other one.
+  # Returns:
+  #     Dictionary with the new keys.
+  def change_dict_naming_convention(self, d, convert_function):
+    new = {}
+    if isinstance(d, str):
+      return d
+    for k, v in d.items():
+      new_v = v
+      if isinstance(v, dict):
+        new_v = self.change_dict_naming_convention(v, convert_function)
+      elif isinstance(v, list):
+        new_v = list()
+        for x in v:
+          new_v.append(self.change_dict_naming_convention(x, convert_function))
+      new[convert_function(k)] = new_v
+    return new
