6f918313228659459106f325cbab6795e20f7599
Yaron Yogev <yayogev@cisco.com>
2016-05-17 13:02:40 +0300

    change order of fetch of vServices and vService vNICs so vServices are fetchd first

--- app/discover/scan_host.py
+++ app/discover/scan_host.py
@@ -4,15 +4,35 @@ from scanner import Scanner
 from scan_instances_root import ScanInstancesRoot
 from scan_pnics_root import ScanPnicsRoot
 from scan_host_network_agents_root import ScanHostNetworkAgentsRoot
-from scan_host_vservices_root import ScanHostVservicesRoot
 from scan_vconnectors_root import ScanVconnectorsRoot
 from scan_vedges_root import ScanVedgesRoot
+from cli_fetch_host_vservices import CliFetchHostVservices
 from cli_fetch_vservice_vnics import CliFetchVserviceVnics
 
 class ScanHost(Scanner, metaclass=Singleton):
   
   def __init__(self):
     super(ScanHost, self).__init__([
+      # creating only top folder for vServices, lower levels are categories
+      # like gateways, DHCPs, etc., and they will be created
+      # while fetching vservices, by specifying master_parent_id.
+      #
+      # this is necessary to allow fetch of vServices to happen
+      # before fetch of vService vNICs
+      {
+        "type": "host_object_type",
+        "fetcher": FolderFetcher("vservices", "host"),
+        "children_scanner": ScanInstancesRoot()
+      },
+      {
+        "type": "vservice",
+        "fetcher": CliFetchHostVservices()
+      },
+      # fetching of vService vNICs is done from host for efficiency
+      {
+        "type": "vnic",
+        "fetcher": CliFetchVserviceVnics()
+      },
       {
         "type": "host_object_type",
         "fetcher": FolderFetcher("instances", "host"),
@@ -38,14 +58,5 @@ class ScanHost(Scanner, metaclass=Singleton):
         "fetcher": FolderFetcher("network_agents", "host",
           "Network agents"),
         "children_scanner": ScanHostNetworkAgentsRoot()
-      },
-      {
-        "type": "host_object_type",
-        "fetcher": FolderFetcher("vservices", "host", "vServices"),
-        "children_scanner": ScanHostVservicesRoot()
-      },
-      {
-        "type": "vnic",
-        "fetcher": CliFetchVserviceVnics()
       }
     ])
