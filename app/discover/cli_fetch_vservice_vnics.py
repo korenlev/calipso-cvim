aac7b63d786c30b69a1d7d789d7f02ad39ac722a
Yaron Yogev <yayogev@cisco.com>
2016-05-22 10:23:05 +0300

    add vservice-vnic link creation

--- app/discover/cli_fetch_vservice_vnics.py
+++ app/discover/cli_fetch_vservice_vnics.py
@@ -75,3 +75,34 @@ class CliFetchVserviceVnics(CliAccess):
       return
     interface["data"] = "\n".join(interface.pop("lines", None))
 
+  def add_links(self):
+    vnics = self.inv.find_items({
+      "environment": self.get_env(),
+      "type": "vnic",
+      "vnic_type": "vservice_vnic"
+    })
+    for v in vnics:
+      self.add_link_for_vnic(v)
+
+  def add_link_for_vnic(self, v):
+    vservice_id = v["parent_id"]
+    vservice_id = vservice_id[:vservice_id.rindex('-')]
+    vservice = self.inv.get_by_id(self.get_env(), vservice_id)
+    source = vservice["_id"]
+    source_id = vservice_id
+    target = v["_id"]
+    target_id = v["id"]
+    link_type = "vservice-vnic"
+    # find related network:
+    # for DHCP, fetch the network ID from the vservice ID
+    if source_id.startswith("qdhcp"):
+      network_id = source_id[source_id.index('-')+1:]
+      network = self.inv.get_by_id(self.get_env(), network_id)
+      link_name = network["name"]
+    else:
+      # for router: find router name in neutron routers
+      link_name = vservice["name"] + "-" + v["mac_address"]
+    state = "up" # TBD
+    link_weight = 0 # TBD
+    self.inv.create_link(self.get_env(), source, source_id, target, target_id,
+      link_type, link_name, state, link_weight)
