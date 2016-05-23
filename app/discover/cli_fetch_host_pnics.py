6d228c5aca3484cdd7fdb2def41ea0f4e8b14603
Yaron Yogev <yayogev@cisco.com>
2016-05-23 15:47:18 +0300

    US399 add pnic-network links

--- app/discover/cli_fetch_host_pnics.py
+++ app/discover/cli_fetch_host_pnics.py
@@ -1,4 +1,5 @@
 from cli_access import CliAccess
+from inventory_mgr import InventoryMgr
 
 import re
 
@@ -6,6 +7,7 @@ class CliFetchHostPnics(CliAccess):
 
   def __init__(self):
     super(CliFetchHostPnics, self).__init__()
+    self.inv = InventoryMgr()
     self.if_header = re.compile('^[-]?(eth[0-9]\S*)\s+(.*)$')
     self.ethtool_attr = re.compile('^\s+([^:]+):\s(.*)$')
     self.regexps = [
@@ -87,5 +89,38 @@ class CliFetchHostPnics(CliAccess):
           interface[attr] = [interface[attr], line.strip()]
         else:
           interface[attr].append(line.strip())
+
+  def add_links(self):
+    pnics = self.inv.find_items({
+      "environment": self.get_env(),
+      "type": "pnic"
+    })
+    for pnic in pnics:
+      self.add_pnic_network_links(pnic)
+
+  def add_pnic_network_links(self, pnic):
+      host = pnic["host"]
+      # find ports for that host, and fetch just the network ID
+      ports = self.inv.find_items({
+        "environment": self.get_env(),
+        "type": "port",
+        "binding:host_id" : host
+      }, {"network_id": 1})
+      networks = {}
+      for port in ports:
+        networks[port["network_id"]] = 1
+      for network_id in networks.keys():
+        network = self.inv.get_by_id(self.get_env(), network_id)
+        source = pnic["_id"]
+        source_id = pnic["id"]
+        target = network["_id"]
+        target_id = network["id"]
+        link_type = "pnic-network"
+        link_name = "Segment-" + str(network["provider:segmentation_id"])
+        state = "up" if pnic["Link detected"] == "yes" else "down"
+        link_weight = 0 # TBD
+        self.inv.create_link(self.get_env(), host,
+          source, source_id, target, target_id,
+          link_type, link_name, state, link_weight)
 
 
