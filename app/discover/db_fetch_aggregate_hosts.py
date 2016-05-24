75e997a302bca7f783dc0b3f65940c851b47b6fc
Yaron Yogev <yayogev@cisco.com>
2016-05-24 17:59:39 +0300

    fix ID of host_ref from aggregates to include name of aggregate and name of host

--- app/discover/db_fetch_aggregate_hosts.py
+++ app/discover/db_fetch_aggregate_hosts.py
@@ -4,8 +4,9 @@ class DbFetchAggregateHosts(DbAccess):
   
   def get(self, id):
     query = """
-      SELECT host AS id, host AS name
-      FROM nova.aggregate_hosts
-      WHERE deleted = 0 AND aggregate_id = %s
+      SELECT CONCAT('aggregate-', a.name, '-', host) AS id, host AS name
+      FROM nova.aggregate_hosts ah
+        JOIN nova.aggregates a ON a.id = ah.aggregate_id
+      WHERE ah.deleted = 0 AND aggregate_id = %s
     """
     return self.get_objects_list_for_id(query, "host", id)
