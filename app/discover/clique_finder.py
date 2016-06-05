e907a1b4dcb2e6b1ab4bfd84aa702ac7a8657f4d
Yaron Yogev <yayogev@cisco.com>
2016-06-05 09:03:24 +0300

    US400 create cliques with each link a separate document

---
+++ app/discover/clique_finder.py
@@ -0,0 +1,70 @@
+from fetcher import Fetcher
+from mongo_access import MongoAccess
+
+from bson.objectid import ObjectId
+
+class CliqueFinder(Fetcher, MongoAccess):
+  
+  def __init__(self, inventory, links, clique_types, cliques):
+    super().__init__()
+    self.inv = inventory
+    self.links = links
+    self.clique_types = clique_types
+    self.cliques = cliques
+  
+  def find_cliques(self):
+    self.log.info("scanning for cliques")
+    clique_types = self.clique_types.find({"environment": self.get_env()})
+    for clique_type in clique_types:
+      self.find_cliques_for_type(clique_type)
+  
+  def find_cliques_for_type(self, clique_type):
+    object_type = clique_type["focal_point_type"]
+    objects_for_focal_point_type = self.inv.find({
+      "environment": self.get_env(),
+      "type": object_type
+    })
+    for o in objects_for_focal_point_type:
+      self.construct_clique_for_focal_point(o, clique_type)
+  
+  def construct_clique_for_focal_point(self, o, clique_type):
+    # keep a hash of nodes in clique that were visited for each type
+    # start from the focal point
+    nodes_of_type = {o["type"]: {str(o["_id"]): 1}}
+    links_used = []
+    clique = {
+      "environment": self.env,
+      "focal_point": o["_id"],
+      "focal_point_type": o["type"]
+    }
+    for link_type in clique_type["links"]:
+      from_type = link_type[:link_type.index("-")]
+      to_type = link_type[link_type.index("-")+1:]
+      if from_type not in nodes_of_type.keys():
+        continue
+      for from_point in nodes_of_type[from_type].keys():
+        matches = self.links.find({
+          "environment": self.env,
+          "link_type": link_type,
+          "source": ObjectId(from_point)
+        })
+        for link in matches:
+          id = link["_id"]
+          if str(id) in links_used:
+            continue
+          links_used.append(str(id))
+          to_point = str(link["target"])
+          if to_type not in nodes_of_type:
+            nodes_of_type[to_type] = {}
+          nodes_of_type[to_type][to_point] = 1
+
+          self.cliques.update_one(
+            {
+              "environment": self.get_env(),
+              "focal_point": clique["focal_point"],
+              "type": link_type,
+              "link_id": id,
+              "name": link["link_name"]
+            },
+            {'$set': self.encode_mongo_keys(clique)},
+            upsert=True)
