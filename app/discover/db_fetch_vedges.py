cbc4f5572a9311f4930b0b693cdd2cadbc694670
Yaron Yogev <yayogev@cisco.com>
2016-06-06 14:09:39 +0300

    started writing code for overlay tunnels data feature, not working at the moment

--- app/discover/db_fetch_vedges.py
+++ app/discover/db_fetch_vedges.py
@@ -22,20 +22,22 @@ class DbFetchVedges(DbAccess, CliAccess, metaclass=Singleton):
         WHERE host = %s AND agent_type = 'Open vSwitch agent'
       """,
       "vedge", host_id)
-    ports = self.fetch_ports(host_id)
+    vsctl_lines = self.run_fetch_lines("ovs-vsctl show", host_id)
+    ports = self.fetch_ports(host_id, vsctl_lines)
     for doc in results:
       doc["name"] = doc["host"] + "-OVS"
       doc["configurations"] = json.loads(doc["configurations"])
       doc["ports"] = ports
+      self.get_overlay_tunnels(doc, vsctl_lines)
     return results
 
-  def fetch_ports(self, host_id):
+  def fetch_ports(self, host_id, vsctl_lines):
     host = self.inv.get_by_id(self.get_env(), host_id)
     host_types = host["host_type"]
     if "Network" not in host_types and "Compute" not in host_types:
       return []
     ports = self.fetch_ports_from_dpctl(host_id)
-    self.fetch_port_tags_from_vsctl(host_id, ports)
+    self.fetch_port_tags_from_vsctl(vsctl_lines, ports)
     return ports
 
   def fetch_ports_from_dpctl(self, host_id):
@@ -62,12 +64,10 @@ class DbFetchVedges(DbAccess, CliAccess, metaclass=Singleton):
   #            tag: 5
   #            Interface "tap9f94d28e-7b"
   #                type: internal
-  def fetch_port_tags_from_vsctl(self, host, ports):
-    cmd = "ovs-vsctl show"
-    lines = self.run_fetch_lines(cmd, host)
+  def fetch_port_tags_from_vsctl(self, vsctl_lines, ports):
     port_line_header_prefix = " " * 8 + "Port "
     port = None
-    for l in lines:
+    for l in vsctl_lines:
       if l.startswith(port_line_header_prefix):
         port = None
         port_name = l[len(port_line_header_prefix):]
@@ -83,6 +83,71 @@ class DbFetchVedges(DbAccess, CliAccess, metaclass=Singleton):
         port["tag"] = l[l.index(":")+2:]
         ports[port["name"]] = port
     return ports
+
+  def get_overlay_tunnels(self, doc, vsctl_lines):
+    if doc["agent_type"] != "Open vSwitch agent":
+      return
+
+    conf = doc["configurations"]
+    if not conf ["tunneling_ip"] or not conf["tunnel_types"]:
+      self.overlay_tunnels_null(doc)
+    else:
+      self.overlay_tunnels_extract(doc, vsctl_lines)
+
+  def overlay_tunnels_null(self, doc):
+    if "bridge_mappings" not in doc["configurations"]:
+      return
+    bridge_mappings = doc["configurations"]["bridge_mappings"]
+    for bridge in bridge_mappings.values():
+      self.find_vedge_ovs_tunnel_pnics_for_bridge(doc, bridge)
+
+  # on every host in the environment, run "ovs-vsctl list-ifaces " + bridge
+  def find_vedge_ovs_tunnel_pnics_for_bridge(self, doc, bridge):
+    cmd = "ovs-vsctl list-ifaces " + bridge
+    host = doc["host"]
+    lines = self.run_fetch_lines(cmd, host)
+    # output we're looking for looks like this:
+    # br-prv--br-eth0
+    # i.e. <bridge>--br-<interface>
+    # we need the interface name to look for the pNIC
+    interfaces = [l[l.index("--br-")+1:] for l in lines]
+    for iname in interfaces:
+      matches = self.inv.find_items({
+        "environment": self.get_env(),
+        "type": "pnic",
+        "host": host,
+        "local_name": iname
+      })
+      if not matches:
+        continue
+      for pnic in matches:
+        self.log.info("found pnic for bridge: " + bridge +
+          ", pnic local_name: " + iname)
+      # found a matching pnic - create its vedge-pnic and pnic-network links
+      # document, and later skip creating them in add_links
+
+      # If any pNIC name exists in the list output
+      # then we have that specific vedge-pnic and pnic-network links
+      # (excluding all others!)
+
+  def overlay_tunnels_extract(self, doc, vsctl_lines):
+    # Read the 'tunneling_ip' IP address
+    conf = doc["configurations"]
+    if "tunneling_ip" not in conf:
+      return
+    tunneling_ip = conf["tunneling_ip"]
+    # Use this ip address to find the associated vedge port from 'ovs-vsctl show'
+    # For example 'local_ip="192.168.2.2"'
+    #  Add that port to the vEdge list of ports.
+    #  For example:
+    #  Port "vxlan-c0a80203" on node-4 in Mirantis-Liberty
+    #  run ifconfig | grep -B 2 192.168.2.2 on that host
+    # to find the pNIC making these tunnels
+    # if that is br-[] or []-br type of pNIC make further command:
+    #   "brctl show juju-br0" (in thundercloud) - to find the associated pNIC (under interfaces) - eth6
+    #   "brctl show br-mesh" (in Mirantis-Liberty) - to find the associated pNIC (under interfaces) - eno33554952.103
+    pass
+
 
   def add_links(self):
     self.log.info("adding link types: vnic-vedge, vconnector-vedge, vedge-pnic")
@@ -166,3 +231,4 @@ class DbFetchVedges(DbAccess, CliAccess, metaclass=Singleton):
     self.inv.create_link(self.get_env(), vedge["host"],
       source, source_id, target, target_id,
       link_type, link_name, state, link_weight)
+
