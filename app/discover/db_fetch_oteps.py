5a0e18c09ffffa436158c440fb1c3576a70ccff3
Yaron Yogev <yayogev@cisco.com>
2016-06-09 15:48:19 +0300

    in OpenStack Icehouse, DB has only IP address for GRE EP, so use values for the host instead, also added missing '-' in name of OTEP object

--- app/discover/db_fetch_oteps.py
+++ app/discover/db_fetch_oteps.py
@@ -22,17 +22,25 @@ class DbFetchOteps(DbAccess, metaclass=Singleton):
     if not vedge["configurations"]["tunnel_types"]:
       return []
     tunnel_type = vedge["configurations"]["tunnel_types"][0]
-    host = vedge["host"]
+    host_id = vedge["host"]
     table_name = "neutron.ml2_" + tunnel_type + "_endpoints"
-    results = self.get_objects_list_for_id(
-      """
+    env_config = self.config.get_env_config()
+    distribution = env_config["distribution"]
+    if distribution == "Canonical-icehouse":
+      # for Icehouse, we only get IP address from the DB, so take the
+      # host IP address and from the host data in Mongo
+      host = self.inv.get_by_id(self.get_env(), host_id)
+      results = [{"host": host_id, "ip_address": host["ip_address"]}]
+    else:
+      results = self.get_objects_list_for_id(
+        """
         SELECT *
         FROM {}
         WHERE host = %s
-      """.format(table_name),
-      "vedge", host)
+        """.format(table_name),
+        "vedge", host_id)
     for doc in results:
-      doc["id"] = host + "otep"
+      doc["id"] = host_id + "-otep"
       doc["name"] = doc["id"]
       doc["overlay_type"] = tunnel_type
       doc["ports"] = vedge["tunnel_ports"] if "tunnel_ports" in vedge else []
