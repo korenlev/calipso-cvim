7a4f796dc70c2cf7f31e70f2ac3a0a273c0670a5
Yaron Yogev <yayogev@cisco.com>
2016-06-20 16:28:39 +0300

    add queue for netron events

--- app/discover/event_listener.py
+++ app/discover/event_listener.py
@@ -6,27 +6,30 @@ logger = get_logger(__name__)
 
 
 class Worker(ConsumerMixin):
-    task_queue = Queue('notifications.info', Exchange('nova', 'topic', durable=False), durable=False)
+  event_queues = [
+    Queue('notifications.info', Exchange('nova', 'topic', durable=False), durable=False),
+    Queue('notifications.info', Exchange('neutron', 'topic', durable=False), durable=False)
+  ]
 
-    def __init__(self, connection):
-        self.connection = connection
+  def __init__(self, connection):
+    self.connection = connection
 
-    def get_consumers(self, Consumer, channel):
-        return [Consumer(queues=[self.task_queue],
-                         accept=['json'],
-                         callbacks=[self.process_task])]
+  def get_consumers(self, Consumer, channel):
+    return [Consumer(queues=self.event_queues,
+      accept=['json'],
+      callbacks=[self.process_task])]
 
-    def process_task(self, body, message):
-        print("RECEIVED MESSAGE: %r" % (body, ))
-        message.ack()
+  def process_task(self, body, message):
+    print("RECEIVED MESSAGE: %r" % (body, ))
+    message.ack()
 
-if __name__ == '__main__':
-    from kombu import Connection
-    from kombu.utils.debug import setup_logging
-    # setup root logger
-    setup_logging(loglevel='DEBUG', loggers=[''])
+    if __name__ == '__main__':
+      from kombu import Connection
+      from kombu.utils.debug import setup_logging
+      # setup root logger
+      setup_logging(loglevel='DEBUG', loggers=[''])
 
-    with Connection('amqp://nova:btE6JPF9@10.56.20.83:5672//') as conn:
+      with Connection('amqp://nova:btE6JPF9@10.56.20.83:5672//') as conn:
         try:
             print(conn)
             worker = Worker(conn)
