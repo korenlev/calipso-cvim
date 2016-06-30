c305d8f7229f1f7f53c8d2a079adbab0adaa7374
Yaron Yogev <yayogev@cisco.com>
2016-06-30 14:33:56 +0300

    US110 add handling of instance rename, add logging, add CLI parameters

--- app/discover/event_handler.py
+++ app/discover/event_handler.py
@@ -1,4 +1,13 @@
-class EventHandler:
+from logger import Logger
+from inventory_mgr import InventoryMgr
+
+class EventHandler(Logger):
+
+  def __init__(self, env, inventory_collection):
+    super().__init__()
+    self.inv = InventoryMgr()
+    self.inv.set_inventory_collection(inventory_collection)
+    self.env = env
 
   def instance_add(self, notification):
     print("instance_add")
@@ -7,7 +16,18 @@ class EventHandler:
     print("instance_delete")
 
   def instance_update(self, notification):
-    pass
+    vals = notification['payload']
+    id = vals['instance_id']
+    instance = self.inv.get_by_id(self.env, id)
+    if not instance:
+      return
+    name = vals['display_name']
+    instance['name'] = name
+    instance['object_name'] = name
+    name_path = instance['name_path']
+    instance['name_path'] = name_path[:name_path.rindex('/') + 1] + name
+    # TBD: fix name_path for descendants
+    self.inv.set(instance)
 
   def instance_down(self, notification):
     pass
@@ -23,4 +43,3 @@ class EventHandler:
 
   def region_update(self, notification):
     pass
-
