7894d79d2e65aa9978e4fbaa4c631330d356eebe
Yaron Yogev <yayogev@cisco.com>
2016-08-30 16:44:53 +0300

    fix duplicate ID between vEdge object and host network agent object also changed quoting to single quote to follow Python style guide

--- app/discover/db_fetch_host_network_agents.py
+++ app/discover/db_fetch_host_network_agents.py
@@ -8,16 +8,19 @@ class DbFetchHostNetworkAgents(DbAccess):
   def __init__(self):
     super().__init__()
     self.inv = InventoryMgr()
+    self.env_config = self.config.get_env_config()
 
   def get(self, id):
     query = """
       SELECT * FROM neutron.agents
       WHERE host = %s
     """
-    host_id = id[:-1*len("-network_agents")]
-    host = self.inv.get_single(self.get_env(), "host", host_id)
-    results = self.get_objects_list_for_id(query, "network_agent", host_id)
+    host_id = id[:-1*len('-network_agents')]
+    results = self.get_objects_list_for_id(query, 'network_agent', host_id)
+    network_plugins = self.env_config['network_plugins']
+    id_prefix = network_plugins[0] if network_plugins else 'network_agent'
     for o in results:
-      o["configurations"] = json.loads(o["configurations"])
-      o["name"] = o["binary"]
+      o['configurations'] = json.loads(o['configurations'])
+      o['name'] = o['binary']
+      o['id'] = id_prefix + '-' + o['id']
     return results
