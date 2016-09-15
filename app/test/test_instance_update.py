a0e222255207e4022d7ba4dc4b013c3129ecca6a
chengli2 <chengli2@cisco.com>
2016-09-15 15:00:13 -0700

    rebuild class TestInstanceUpdate by inheriting TestEvent

--- app/test/test_instance_update.py
+++ app/test/test_instance_update.py
@@ -1,27 +1,20 @@
 import unittest
-from discover.configuration import Configuration
-from discover.event_handler import EventHandler
-from test.get_args import GetArgs
 from test.test_data.event_payload_instance_update import EVENT_PAYLOAD_INSTANCE_UPDATE
+from test.test_event import TestEvent
 
 
-class TestInstanceUpdate(unittest.TestCase):
-    def setUp(self):
-        self.arg_getter = GetArgs()
-        self.args = self.arg_getter.get_args()
-
-        self.conf = Configuration(self.args.mongo_config)
-        self.conf.use_env(self.args.env)
-        self.handler = EventHandler(self.args.env, self.args.inventory)
-        self.values = EVENT_PAYLOAD_INSTANCE_UPDATE
+class TestInstanceUpdate(TestEvent):
 
     def test_handle_normal_situation(self):
+        self.values = EVENT_PAYLOAD_INSTANCE_UPDATE
         payload = self.values['payload']
         id = payload['instance_id']
         new_name = payload['display_name']
 
         # get instance document
         instance = self.handler.inv.get_by_id(self.args.env, id)
+        self.assertIsNot(instance, [])
+
         name_path = instance['name_path']
         new_name_path = name_path[:name_path.rindex('/') + 1] + new_name
 
