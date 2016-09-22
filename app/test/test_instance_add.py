4f4e5b8afa4264130f7a383c672d2b1d203db992
chengli2 <chengli2@cisco.com>
2016-09-22 14:48:55 -0700

    use InventoryMrg::delete instead of handler::instance_delete, and check doc after deleting.

--- app/test/test_instance_add.py
+++ app/test/test_instance_add.py
@@ -7,20 +7,23 @@ class TestInstanceAdd(TestEvent):
     def test_handle_instance_add(self):
         self.values = EVENT_PAYLOAD_INSTANCE_ADD
         payload = self.values['payload']
-        _id = payload['instance_id']
+        id = payload['instance_id']
         host_id = payload['host']
 
         # check instance document
-        instance = self.handler.inv.get_by_id(self.env, _id)
+        instance = self.handler.inv.get_by_id(self.env, id)
         if instance:
             self.handler.log.info('instance document has existed, delete it first.')
-            self.handler.instance_delete(payload)
+            self.handler.inv.delete('inventory', {'id': id})
+
+            instance = self.handler.inv.get_by_id(self.env, id)
+            self.assertEqual(instance, [])
 
         # add instance into database
         self.handler.instance_add(payload)
 
         # check instance document
-        instance = self.handler.inv.get_by_id(self.env, _id)
+        instance = self.handler.inv.get_by_id(self.env, id)
         self.assertNotEqual(instance, [])
 
         # check host document
@@ -28,4 +31,6 @@ class TestInstanceAdd(TestEvent):
         self.assertNotEqual(host, [])
 
         # Delete the document after test.
-        self.handler.instance_delete(payload)
+        self.handler.inv.delete('inventory', {'id': id})
+        instance = self.handler.inv.get_by_id(self.env, id)
+        self.assertEqual(instance, [])
