f56addd89dac10e2e37c2f87676f9ffb10ba0888
chengli2 <chengli2@cisco.com>
2016-10-04 17:14:34 -0700

    add logic for scanning network

--- app/discover/events/event_subnet_add.py
+++ app/discover/events/event_subnet_add.py
@@ -1,5 +1,7 @@
 from discover.fetcher import Fetcher
 from discover.inventory_mgr import InventoryMgr
+from discover.scan_network import ScanNetwork
+from discover.scan_networks_root import ScanNetworksRoot
 
 
 class EventSubnetAdd(Fetcher):
@@ -16,11 +18,22 @@ class EventSubnetAdd(Fetcher):
             self.log.info('network document does not exist, aborting subnet add')
             return None
 
-        # build subnet document for adding network
-        subnet_name = subnet['name']
-        subnet_document = {subnet_name: subnet}
-        network_document['cidrs'] = [subnet['cidr']]
-        network_document['subnets'] = subnet_document
+        # Check DHCP enable, if true, scan network.
+        if subnet['enable_dhcp'] == True:
+            network_scanner = ScanNetwork()
+            network_scanner.set_env(env)
+            # network_root_doc = self.inv.get_by_id(env, network_document['parent_id'])
+            # print(network_root_doc)
+            network_scanner.scan(network_document)
+            network_scanner.scan_from_queue()
+            network_scanner.scan_links()
+            network_scanner.scan_cliques()
+        else:
+            # build subnet document for adding network
+            subnet_name = subnet['name']
+            subnet_document = {subnet_name: subnet}
+            network_document['cidrs'] = [subnet['cidr']]
+            network_document['subnets'] = subnet_document
 
-        # update network document.
-        self.inv.set(network_document)
+            # update network document.
+            self.inv.set(network_document)
