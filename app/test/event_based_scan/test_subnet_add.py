e015a09384c29bf21ce6b90038a2765cdda512f4
chengli2 <chengli2@cisco.com>
2016-10-18 16:11:38 -0700

    use Mock to replace get data of port and VNICs

--- app/test/event_based_scan/test_subnet_add.py
+++ app/test/event_based_scan/test_subnet_add.py
@@ -1,4 +1,6 @@
+from unittest.mock import MagicMock
 from discover.api_access import ApiAccess
+from discover.events.event_subnet_add import EventSubnetAdd
 from test.event_based_scan.test_data.event_payload_subnet_add import EVENT_PAYLOAD_SUBNET_ADD, EVENT_PAYLOAD_NETWORK_ADD, \
     EVENT_PAYLOAD_REGION
 from test.event_based_scan.test_event import TestEvent
@@ -13,7 +15,8 @@ class TestSubnetAdd(TestEvent):
         self.payload = self.values['payload']
         self.subnet = self.payload['subnet']
         self.subnet_id = self.subnet['id']
-        self.network_id = self.subnet['network_id']
+        # self.item_id for tearDown()
+        self.item_id = self.network_id = self.subnet['network_id']
 
         network_document = self.handler.inv.get_by_id(self.env, self.network_id)
         if network_document:
@@ -26,23 +29,19 @@ class TestSubnetAdd(TestEvent):
             network_document = self.handler.inv.get_by_id(self.env, self.network_id)
             self.assertNotEqual(network_document, [])
 
-            # todo add port data to the OpenStack DB. To create dhcp namespace as well.
-
         # check region data.
         if len(ApiAccess.regions) == 0:
             ApiAccess.regions = EVENT_PAYLOAD_REGION
 
+        # Mock function instead of get children data. They should be test in their unit test.
         # add subnet document for updating network
-        self.handler.subnet_create(self.values)
+        handler = EventSubnetAdd()
+        handler.add_children_documents = MagicMock()
+        handler.handle(self.env, self.values)
 
         # check network document
         network_document = self.handler.inv.get_by_id(self.env, self.network_id)
         self.assertIn(self.subnet['cidr'], network_document['cidrs'])
         self.assertIn(self.subnet['name'], network_document['subnets'])
 
-
-    # Delete the document after test.
-    def tearDown(self):
-        self.handler.inv.delete('inventory', {'id': self.network_id})
-        network_document = self.handler.inv.get_by_id(self.env, self.network_id)
-        self.assertEqual(network_document, [])
+        #tearDown method has been deployed in class testEvent.
