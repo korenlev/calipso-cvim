5fb737ed95b3b8b7c97a03812f30f9a773686720
chengli2 <chengli2@cisco.com>
2016-11-01 16:39:03 -0700

    add unit test for subnet deleting handler

---
+++ app/test/event_based_scan/test_subnet_delete.py
@@ -0,0 +1,44 @@
+from test.event_based_scan.test_event import TestEvent
+from test.event_based_scan.test_data.event_payload_subnet_delete import EVENT_PAYLOAD_SUBNET_DELETE, \
+    EVENT_PAYLOAD_NETWORK
+
+
+class TestSubnetDelete(TestEvent):
+
+    def test_handle_subnet_delete(self):
+        self.values = EVENT_PAYLOAD_SUBNET_DELETE
+        self.subnet_id = self.values['payload']['subnet_id']
+        self.network_doc = EVENT_PAYLOAD_NETWORK
+        self.network_id = self.network_doc['id']
+        self.item_id = self.network_id
+
+        self.subnet_name = None
+        self.cidr = None
+
+        for subnet in self.network_doc['subnets'].values():
+            if subnet['id'] == self.subnet_id:
+                self.subnet_name = subnet['name']
+                self.cidr = subnet['cidr']
+                break
+
+        # add document for subnet deleting test.
+        self.handler.inv.set(self.network_doc)
+        network_document = self.handler.inv.get_by_id(self.env, self.network_id)
+        self.assertNotEqual(network_document, [], "add network document failed")
+
+        # delete subnet
+        self.handler.subnet_delete(self.values)
+
+        network_document = self.handler.inv.get_by_id(self.env, self.network_id)
+        self.assertNotIn(self.subnet_id, network_document['subnets_id'])
+        self.assertNotIn(self.cidr, network_document['cidrs'])
+        self.assertNotIn(self.subnet_name, network_document['subnets'])
+
+        # assert children documents
+        vservice_dhcp_id = 'qdhcp-' + network_document['id']
+        dhcp_doc = self.handler.inv.get_by_id(self.env, vservice_dhcp_id)
+        self.assertEqual(dhcp_doc,[])
+
+        vnic_parent_id = vservice_dhcp_id + '-vnics'
+        vnic = self.handler.inv.get_by_field(self.env, 'vnic', 'parent_id', vnic_parent_id, get_single=True)
+        self.assertEqual(vnic, [])
