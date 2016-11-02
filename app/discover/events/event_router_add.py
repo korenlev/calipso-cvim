193d06ab9d67bbea7c0162c935021d05f3ac24ec
chengli2 <chengli2@cisco.com>
2016-11-02 14:11:04 -0700

    refator EventRouterAdd, separate code into different methods.

--- app/discover/events/event_router_add.py
+++ app/discover/events/event_router_add.py
@@ -17,8 +17,8 @@ class EventRouterAdd(Fetcher):
     def add_router_document(self, env, network_id, router_doc, host):
         router_doc["children_url"] = "/osdna_dev/discover.py?type=tree&id=%s" % router_doc['id']
         router_doc["environment"] = env
-        router_doc["id_path"] = host['id_path']+"/%s-vservices/%s-vservices-routers/%s" % (host['id'], host['id'],
-                                                                                           router_doc['id'])
+        router_doc["id_path"] = host['id_path'] + "/%s-vservices/%s-vservices-routers/%s" % (host['id'], host['id'],
+                                                                                             router_doc['id'])
         router_doc['last_scanned'] = datetime.datetime.utcnow()
         router_doc['name_path'] = host['name_path'] + "/Vservices/Gateways/%s" % router_doc['name']
         router_doc['network'] = [network_id]
@@ -28,6 +28,32 @@ class EventRouterAdd(Fetcher):
         router_doc['type'] = "vservice"
 
         self.inv.set(router_doc)
+
+    def add_children_documents(self, env, project_id, network_id, host, router_doc):
+
+        network_document = self.inv.get_by_id(env, network_id)
+        network_name = network_document['name']
+
+        # add port for binding to vservice:router
+        subnet_handler = EventSubnetAdd()
+        ports_folder = self.inv.get_by_id(env, network_id + '-ports')
+        if not ports_folder:
+            self.log.info("Ports_folder not found.")
+            subnet_handler.add_ports_folder(env, project_id, network_id, network_name)
+        subnet_handler.add_port_document(env, project_id, network_id, network_name, router_doc['gw_port_id'])
+
+        # add vnics folder and vnic document
+        port_handler = EventPortAdd()
+        port_handler.add_vnics_folder(env, host, network_id, network_name, type="router",
+                                      router_name=router_doc['name'])
+
+        port_handler.add_vnic_document(env, host, network_id, network_name, type="router",
+                                       router_name=router_doc['name'])
+
+    def update_links_and_cliques(self, fetchers, scanner):
+        for fetcher in fetchers:
+            fetcher.add_links()
+        scanner.scan_cliques()
 
     def handle(self, env, values):
         router = values['payload']['router']
@@ -41,38 +67,9 @@ class EventRouterAdd(Fetcher):
 
         router_doc = fetcher.get_vservice(host_id, router_id)
         network_id = router['external_gateway_info']['network_id']
-        port_id = router_doc['gw_port_id']
         self.add_router_document(env, network_id, router_doc, host)
-
-        # Add children documents of port and vnic.
-        # after creating a router, a namespace should be created, but it does not.
-        # But we still write down the right logic here.
-        network_document = self.inv.get_by_id(env, network_id)
-        network_name = network_document['name']
-
-        # add port for binding to vservice:router
-        subnet_handler = EventSubnetAdd()
-        ports_folder = self.inv.get_by_id(env, network_id+ '-ports')
-        if not ports_folder:
-            self.log.info("Ports_folder not found.")
-            subnet_handler.add_ports_folder(env, project_id, network_id, network_name)
-        subnet_handler.add_port_document(env, project_id, network_id, network_name, port_id)
-
-        # add vnics folder
-        port_handler = EventPortAdd()
-        port_handler.add_vnics_folder(env, host, network_id, network_name, type="router",
-                                      router_name=router_doc['name'])
-
-        # add vnic docuemnt.
-        port_handler.add_vnic_document(env, host, network_id, network_name,
-                                       type="router", router_name=router_doc['name'])
-
+        self.add_children_documents(env, project_id, network_id, host, router_doc)
 
         # scan links and cliques
-        self.log.info("scanning links")
-        for fetcher in [FindLinksForVserviceVnics()]:
-            fetcher.add_links()
-
-        scanner = ScanNetwork()
-        scanner.scan_cliques()
+        self.update_links_and_cliques([FindLinksForVserviceVnics()], ScanNetwork())
         self.log.info("Finished router added.")
