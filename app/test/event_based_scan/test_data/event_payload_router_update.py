ade2b98f29baed00b51c883a8476fb0993da427c
chengli2 <chengli2@cisco.com>
2016-11-03 14:43:13 -0700

    correct test data for router update

--- app/test/event_based_scan/test_data/event_payload_router_update.py
+++ app/test/event_based_scan/test_data/event_payload_router_update.py
@@ -9,7 +9,7 @@ EVENT_PAYLOAD_ROUTER_UPDATE = {
     '_context_user_identity': '13baa553aae44adca6615e711fd2f6d9 75c0eb79ff4a42b0ae4973c8375ddf40 - - -',
     '_context_timestamp': '2016-10-28 20:29:35.548123', 'message_id': '42c0ca64-cea1-4c89-a059-72abf7990c40',
     'payload': {
-        'router': {'id': '0ecbc349-a928-47e2-ab49-86a27df6be7f', 'tenant_id': '75c0eb79ff4a42b0ae4973c8375ddf40',
+        'router': {'id': 'bde87a5a-7968-4f3b-952c-e87681a96078', 'tenant_id': '75c0eb79ff4a42b0ae4973c8375ddf40',
                    'ha': False, 'distributed': False, 'name': 'abc', 'status': 'ACTIVE', 'external_gateway_info': None,
                    'admin_state_up': True, 'routes': []}}, '_context_resource_uuid': None,
     'event_type': 'router.update.end', '_context_project_name': 'OSDNA-project', 'priority': 'INFO',
@@ -19,13 +19,45 @@ EVENT_PAYLOAD_ROUTER_UPDATE = {
     '_context_show_deleted': False, '_context_tenant_name': 'OSDNA-project', 'publisher_id': 'network.node-6.cisco.com',
     'timestamp': '2016-10-28 20:29:39.986161'}
 
+ROUTER_DOCUMENT = {
+    "admin_state_up": True,
+    "children_url": "/osdna_dev/discover.py?type=tree&id=qrouter-bde87a5a-7968-4f3b-952c-e87681a96078",
+    "enable_snat": 1,
+    "environment": "Mirantis-Liberty-CL",
+    "gw_port_id": None,
+    "host": "node-6.cisco.com",
+    "id": "qrouter-bde87a5a-7968-4f3b-952c-e87681a96078",
+    "id_path": "/Mirantis-Liberty-CL/Mirantis-Liberty-CL-regions/RegionOne/RegionOne-availability_zones/internal" +
+               "/node-6.cisco.com/node-6.cisco.com-vservices/node-6.cisco.com-vservices-routers/qrouter-bde87a5a" +
+               "-7968-4f3b-952c-e87681a96078",
+    "last_scanned": 0,
+    "local_service_id": "qrouter-bde87a5a-7968-4f3b-952c-e87681a96078",
+    "master_parent_id": "node-6.cisco.com-vservices",
+    "master_parent_type": "vservices_folder",
+    "name": "1234",
+    "name_path": "/Mirantis-Liberty-CL/Regions/RegionOne/Availability Zones/internal/node-6.cisco.com/" +
+                 "Vservices/Gateways/router-1234",
+    "network": [
+        "c64adb76-ad9d-4605-9f5e-bd6dbe325cfb"
+    ],
+    "object_name": "router-1234",
+    "parent_id": "node-6.cisco.com-vservices-routers",
+    "parent_text": "Gateways",
+    "parent_type": "vservice_routers_folder",
+    "service_type": "router",
+    "show_in_tree": True,
+    "status": "ACTIVE",
+    "tenant_id": "75c0eb79ff4a42b0ae4973c8375ddf40",
+    "type": "vservice"
+}
+
 EVENT_PAYLOAD_ROUTER_SET_GATEWAY = {
     'publisher_id': 'network.node-6.cisco.com',
     '_context_request_id': 'req-79d53b65-47b8-46b2-9a72-3f4031e2d605',
     '_context_project_name': 'OSDNA-project', '_context_show_deleted': False,
     '_context_user_name': 'admin', '_context_timestamp': '2016-11-02 21:44:31.156447',
     '_context_user': '13baa553aae44adca6615e711fd2f6d9', 'payload': {
-        'router': {'id': '7d086d81-9bd4-4839-bec3-97f6e8051b61', 'admin_state_up': True, 'routes': [],
+        'router': {'id': 'bde87a5a-7968-4f3b-952c-e87681a96078', 'admin_state_up': True, 'routes': [],
                    'status': 'ACTIVE', 'ha': False, 'name': 'test_namespace', 'distributed': False,
                    'tenant_id': '75c0eb79ff4a42b0ae4973c8375ddf40', 'external_gateway_info': {'external_fixed_ips': [
                 {'ip_address': '172.16.0.144', 'subnet_id': 'a5336853-cbc0-49e8-8401-a093e8bab7bb'}],
@@ -64,7 +96,7 @@ EVENT_PAYLOAD_ROUTER_DEL_GATEWAY = {
     'priority': 'INFO',
     '_context_project_id': '75c0eb79ff4a42b0ae4973c8375ddf40', 'message_id': '5272cd90-7151-4d13-8c1f-e8ff2db773a1',
     '_context_project_name': 'OSDNA-project', '_context_resource_uuid': None, 'payload': {
-        'router': {'id': 'ed49f502-3b90-4104-8d3c-b1a3025a7627', 'external_gateway_info': None, 'distributed': False,
+        'router': {'id': 'bde87a5a-7968-4f3b-952c-e87681a96078', 'external_gateway_info': None, 'distributed': False,
                    'name': 'TEST_AAA', 'routes': [], 'ha': False, 'admin_state_up': True,
                    'tenant_id': '75c0eb79ff4a42b0ae4973c8375ddf40', 'status': 'ACTIVE'}},
     '_context_request_id': 'req-d7e73189-4709-4234-8b4c-fb6b4dc2017b'}
