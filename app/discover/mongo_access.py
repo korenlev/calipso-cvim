0ec09e5d4a50fcefad164c743c6bf8404123dc0c
Yaron Yogev <yayogev@cisco.com>
2016-11-15 18:15:00 +0200

    support authentication with user+password+auth_db

--- app/discover/mongo_access.py
+++ app/discover/mongo_access.py
@@ -48,11 +48,22 @@ class MongoAccess(Logger):
             except Exception as e:
                 self.log.error("failed to open config file: " + config_file)
                 raise
+        self.prepare_connect_uri()
         MongoAccess.client = MongoClient(
             self.connect_params["server"],
             self.connect_params["port"]
         )
         MongoAccess.db = MongoAccess.client.osdna
+
+    def prepare_connect_uri(self):
+        params = self.connect_params
+        uri = 'mongodb://'
+        if 'password' in params:
+            uri = uri + params['user'] + ':' + params['password'] + '@'
+        uri = uri + params['server']
+        if 'auth_db' in params:
+            uri = uri + '/' + params['auth_db']
+        self.connect_params['server'] = uri
 
     def encode_dots(self, s):
         return s.replace(".", "[dot]")
