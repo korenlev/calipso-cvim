48727fd6cb3570d6c33346a5abe1fe6d5eecb6d2
Yaron Yogev <yayogev@cisco.com>
2016-11-24 15:12:03 +0200

    US1419 git pnics from /sys/class/net; no need to look for interface name in ifconfig output

--- app/discover/cli_fetch_host_pnics_ovs.py
+++ app/discover/cli_fetch_host_pnics_ovs.py
@@ -9,7 +9,6 @@ class CliFetchHostPnicsOvs(Fetcher, CliAccess):
     def __init__(self):
         super().__init__()
         self.inv = InventoryMgr()
-        self.if_header = re.compile('^[-]?(eth[0-9]\S*|eno\S*)\s+(.*)$')
         self.ethtool_attr = re.compile('^\s+([^:]+):\s(.*)$')
         self.regexps = [
             {"mac_address": re.compile('^.*\sHWaddr\s(\S+)(\s.*)?$')},
@@ -22,10 +21,7 @@ class CliFetchHostPnicsOvs(Fetcher, CliAccess):
 
     def get(self, id):
         host_id = id[:id.rindex("-")]
-        cmd = "ip -d link show | " + \
-              "grep '^[0-9]\+: \(eth\|eno\)' | " + \
-              "sed 's/^[^:]*: *//' | " + \
-              "sed 's/:.*//'"
+        cmd = 'ls -l /sys/class/net | grep ^l | grep -v "/virtual/"'
         host = self.inv.get_by_id(self.get_env(), host_id)
         if not host:
             self.log.error("CliFetchHostPnics: host not found: " + host_id)
@@ -37,12 +33,13 @@ class CliFetchHostPnicsOvs(Fetcher, CliAccess):
         host_types = host["host_type"]
         if "Network" not in host_types and "Compute" not in host_types:
             return []
-        interfaces_names = self.run_fetch_lines(cmd, host_id)
+        interface_lines = self.run_fetch_lines(cmd, host_id)
         interfaces = []
-        for i in interfaces_names:
+        for line in interface_lines:
+            interface_name = line[line.rindex('/')+1:]
             # run ifconfig with specific interface name,
             # since running it with no name yields a list without inactive pNICs
-            interface = self.find_interface_details(host_id, i)
+            interface = self.find_interface_details(host_id, interface_name)
             if interface:
                 interfaces.append(interface)
         return interfaces
@@ -50,23 +47,36 @@ class CliFetchHostPnicsOvs(Fetcher, CliAccess):
     def find_interface_details(self, host_id, interface_name):
         lines = self.run_fetch_lines("ifconfig " + interface_name, host_id)
         interface = None
+        status_up = None
         for line in lines:
-            matches = self.if_header.match(line)
-            if matches:
-                name = matches.group(1).strip(":")
-                line_remainder = matches.group(2)
-                id = interface_name
-                interface = {
-                    "host": host_id,
-                    "name": id,
-                    "local_name": interface_name,
-                    "lines": []
-                }
-                self.handle_line(interface, line_remainder)
+            tokens = None
+            matches = False
+            if interface == None:
+                tokens = line.split()
+                name = tokens[0].strip('- :')
+                if name == interface_name:
+                    line_remainder = line.strip('-')[len(interface_name)+2:]
+                    line_remainder = line_remainder.strip(' :')
+                    id = interface_name
+                    interface = {
+                        "host": host_id,
+                        "name": id,
+                        "local_name": interface_name,
+                        "lines": []
+                    }
+                    self.handle_line(interface, line_remainder)
+            if status_up == None:
+                if tokens == None:
+                    tokens = line.split()
+                if 'BROADCAST' in tokens:
+                    status_up = 'UP' in tokens
             else:
                 if interface:
                     self.handle_line(interface, line)
         self.set_interface_data(interface)
+        interface['state'] = 'UP' if status_up else 'DOWN'
+        if 'id' not in interface:
+            interface['id'] = interface_name + '-unknown_mac'
         return interface
 
     def handle_line(self, interface, line):
