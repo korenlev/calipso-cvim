fe7d5ffe90e9deab335d9bf6fcf908dd494976be
yayogev <yayogev@cisco.com>
2016-12-12 18:31:49 +0200

    fix lint errors

--- app/discover/db_fetch_instances.py
+++ app/discover/db_fetch_instances.py
@@ -10,20 +10,20 @@ class DbFetchInstances(DbAccess):
             instances_hash[doc["id"]] = doc
 
         query = """
-      SELECT DISTINCT i.uuid AS id, i.display_name AS name,
-        i.host AS host, host_ip AS ip_address,
-        network_info, project_id,
-        IF(p.name IS NULL, "Unknown", p.name) AS project
-      FROM nova.instances i
-        LEFT JOIN keystone.project p ON p.id = i.project_id
-        JOIN nova.instance_info_caches ic ON i.uuid = ic.instance_uuid
-        JOIN nova.compute_nodes cn ON i.node = cn.hypervisor_hostname
-      WHERE i.deleted = 0
-    """
+          SELECT DISTINCT i.uuid AS id, i.display_name AS name,
+            i.host AS host, host_ip AS ip_address,
+            network_info, project_id,
+            IF(p.name IS NULL, "Unknown", p.name) AS project
+          FROM nova.instances i
+            LEFT JOIN keystone.project p ON p.id = i.project_id
+            JOIN nova.instance_info_caches ic ON i.uuid = ic.instance_uuid
+            JOIN nova.compute_nodes cn ON i.node = cn.hypervisor_hostname
+          WHERE i.deleted = 0
+        """
         results = self.get_objects_list(query, "instance")
         for result in results:
             id = result["id"]
-            if not id in instances_hash:
+            if id not in instances_hash:
                 continue
             self.build_instance_details(result)
             doc = instances_hash[id]
@@ -33,7 +33,8 @@ class DbFetchInstances(DbAccess):
         network_info_str = result.pop("network_info", None)
         result["network_info"] = json.loads(network_info_str)
 
-        # add network as an array to allow constraint checking when building clique
+        # add network as an array to allow constraint checking
+        # when building clique
         networks = []
         for net in result["network_info"]:
             if "network" not in net or "id" not in net["network"]:
@@ -45,6 +46,6 @@ class DbFetchInstances(DbAccess):
         result["network"] = networks
 
         result["type"] = "instance"
-        result["parent_type"] = "instances_folder";
-        result["parent_id"] = result["host"] + "-instances";
+        result["parent_type"] = "instances_folder"
+        result["parent_id"] = result["host"] + "-instances"
         result["in_project-" + result.pop("project", None)] = "1"
