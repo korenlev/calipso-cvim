09515167db12854a7eb7125658eaf0d54b77af51
yayogev <yayogev@cisco.com>
2016-12-26 17:36:54 +0200

    added merge of checks config into client.json file

--- app/monitoring/setup/monitoring_check_handler.py
+++ app/monitoring/setup/monitoring_check_handler.py
@@ -9,19 +9,30 @@ class MonitoringCheckHandler(MonitoringHandler):
         self.inv = InventoryMgr()
 
     # add monitoring setup on remote host for given object
-    def create_monitoring_for_object(self, o, config):
+    def create_monitoring_for_object(self, o, values):
+        self.replacements.update(self.env_monitoring_config)
+        self.replacements.update(values)
         if 'host' in o:
             host = self.inv.get_by_id(self.env, o['host'])
             if host and 'ip_address' in host:
                 self.replacements['client_ip'] = host['ip_address']
-        config.update(self.env_monitoring_config)
         config_folder = self.env_monitoring_config['config_folder']
         file_type = 'client_check_' + o['type'] + '.json'
         host = o['host']
         directory = self.make_directory(config_folder + '/host/' + host)
         content = self.prepare_config_file(
-            'client.json',
-            {'side': 'client', 'type': file_type},
-            config)
-        full_path = directory + '/' + file_type
-        self.write_config_file(full_path, 'client.json', host, content)
+            file_type,
+            {'side': 'client', 'type': file_type})
+        # need to put this content inside client.json file
+        client_file = 'client.json'
+        host = o['host']
+        client_file_content = self.get_config_from_db(host, client_file)
+        # merge checks attribute from current content into client.json
+        checks = client_file_content['config']['checks'] \
+            if 'checks' in client_file_content['config'] \
+            else {}
+        checks.update(content['config']['checks'])
+        client_file_content['config']['checks'] = checks
+        content = client_file_content
+        full_path = directory + '/' + client_file
+        self.write_config_file(client_file, full_path, host, content)
