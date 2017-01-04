1ab25857ae0af97bcc98d1db42f6a7524f408926
yayogev <yayogev@cisco.com>
2017-01-04 09:22:17 +0200

    for 'bridge' attribute, use vconnector object_name

--- app/monitoring/setup/monitoring_link_vnic_vconnector.py
+++ app/monitoring/setup/monitoring_link_vnic_vconnector.py
@@ -8,7 +8,6 @@ class MonitoringLinkVnicVconnector(MonitoringCheckHandler):
 
     # add monitoring setup for remote host
     def create_setup(self, link):
-        vconnector_id = link['target_id']
         vnic = self.inv.get_by_id(self.env, link['source_id'])
         if not vnic:
             self.log.error('could not find vnic for vnic-vconnector link')
@@ -16,10 +15,14 @@ class MonitoringLinkVnicVconnector(MonitoringCheckHandler):
         if 'mac_address' not in vnic:
             self.log.error('could not find MAC address in vNIC: ' + vnic['id'])
             return
+        vconnector = self.inv.get_by_id(self.env, link['target_id'])
+        if not vnic:
+            self.log.error('could not find vconnector for vnic-vconnector link')
+            return
         values = {
             'linktype': 'vnic-vconnector',
-            'fromobjid': link['source_id'],
-            'toobjid': vconnector_id,
-            'bridge': vconnector_id,
+            'fromobjid': vnic['id'],
+            'toobjid': vconnector['id'],
+            'bridge': vconnector['object_name'],
             'mac_address': vnic['mac_address']}
         self.create_monitoring_for_object(link, values)
