dd77f98316f310cfd5661d638745276b5f515957
yayogev <yayogev@cisco.com>
2017-01-17 19:51:19 +0200

    deploying of monitoring setup changes after scan is finished

--- app/monitoring/setup/monitoring_handler.py
+++ app/monitoring/setup/monitoring_handler.py
@@ -4,19 +4,25 @@ import copy
 import json
 import os
 import pymongo
+import shutil
 import socket
+import subprocess
 
 from boltons.iterutils import remap
 
+from discover.cli_access import CliAccess
 from discover.configuration import Configuration
 from discover.inventory_mgr import InventoryMgr
 from discover.mongo_access import MongoAccess
 from discover.ssh_conn import SshConn
 from utils.deep_merge import remerge
+from utils.binary_converter import BinaryConverter
 
 
-class MonitoringHandler(MongoAccess):
+class MonitoringHandler(MongoAccess, CliAccess, BinaryConverter):
     PRODUCTION_CONFIG_DIR = '/etc/sensu/conf.d'
+
+    pending_changes = {}
 
     def __init__(self, mongo_conf_file, env):
         super().__init__(mongo_conf_file)
@@ -54,6 +60,7 @@ class MonitoringHandler(MongoAccess):
         content = {}
         for doc in docs:
             content.update(doc)
+        self.replacements['app_path'] = self.config.env_config['app_path']
         config = self.content_replace({'config': content['config']})
         return config
 
@@ -122,27 +129,103 @@ class MonitoringHandler(MongoAccess):
         content_json = json.dumps(content['config'], sort_keys=True, indent=4)
         content_json = content_json + '\n'
         # always write the file locally first
-        local_path = self.get_config_dir() + '/' + file_name
+        local_dir = self.make_directory(self.get_config_dir() + '/' + sub_dir)
+        local_path = local_dir + '/' + file_name
         self.write_to_local_host(local_path, content_json)
-        if not self.in_debug():
-            file_path = self.PRODUCTION_CONFIG_DIR + '/' + file_name
-            if host == self.local_host:
+        self.track_pending_host_setup_changes(host, file_type, local_path,
+                                              sub_dir)
+
+    def track_pending_host_setup_changes(self, host, file_type, local_path,
+                                         sub_dir):
+        if host not in self.pending_changes:
+            self.pending_changes[host] = {}
+        if file_type not in self.pending_changes[host]:
+            self.pending_changes[host][file_type] = {
+                "host": host,
+                "file_type": file_type,
+                "local_path": local_path,
+                "sub_dir": sub_dir
+            }
+
+    def handle_pending_setup_changes(self):
+        self.log.info('applying monitoring setup')
+        for host, host_changes in self.pending_changes.items():
+            self.handle_pending_host_setup_changes(host_changes)
+        self.log.info('done applying monitoring setup')
+
+    def handle_pending_host_setup_changes(self, host_changes):
+        if self.in_debug():
+            return
+        host = None
+        for file_type, changes in host_changes.items():
+            host = changes['host']
+            self.log.debug('applying monitoring setup changes ' +
+                           'for host ' + host + ', file type: ' + file_type)
+            is_local_host = host == self.local_host
+            file_path = self.PRODUCTION_CONFIG_DIR + '/' + file_type
+            if is_local_host:
                 # write to production configuration directory on local host
                 self.make_directory(self.PRODUCTION_CONFIG_DIR)
-                self.write_to_local_host(file_path, content_json)
+                shutil.copy(changes['local_path'], file_path)
             else:
-                # write to remote host - use sftp
-                self.write_to_remote_host(host, local_path, file_name)
+                # write to remote host prepare dir - use sftp
+                self.write_to_remote_host(host, changes['local_path'],
+                                          changes['file_type'])
+        if not is_local_host:
+            # copy the files to remote target config directory
+            self.make_remote_dir(host, self.PRODUCTION_CONFIG_DIR)
+            local_path = changes['local_path']
+            local_dir = local_path[:local_path.rindex('/')]
+            self.move_setup_files_to_remote_host(host, local_dir)
+            # restart the Sensu client on the remote host,
+            # so it takes the new setup
+            self.run('sudo /etc/init.d/sensu-service client restart',
+                     ssh_to_host=host)
+
+    def run_cmd_locally(self, cmd):
+        try:
+            subprocess.popen(cmd.split(),
+                             shell=True,
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        except subprocess.CalledProcessError as e:
+            print("Error running command: " + cmd +
+                  ", output: " + self.binary2str(e.output) + "\n")
+
+    def move_setup_files_to_remote_host(self, host, local_dir):
+        if self.is_gateway_host(host):
+            # do a simple copy on the gateway host
+            self.run('cp ' + local_dir + '/* ' + self.PRODUCTION_CONFIG_DIR,
+                     enable_cache=False)
+            return
+        # need to scp the files from the gateway host to the target host
+        ssh = SshConn(host)
+        remote_path = ssh.get_user() + '@' + host + ':' + \
+            self.PRODUCTION_CONFIG_DIR + '/'
+        self.make_remote_dir(host, remote_path)
+        self.run_on_gateway('scp ' + local_dir + '/* ' + remote_path,
+                            ssh_to_host=host, enable_cache=False)
+
+    def make_remote_dir(self, host, path, path_is_file=False):
+        # make sure we have write permissions in target directories
+        dir = path
+        if path_is_file:
+            dir = dir[:dir.rindex('/')]
+        ssh = SshConn(host)
+        cmd = 'sudo mkdir -p ' + dir + \
+            ' && sudo chown -R ' + ssh.get_user() + ' ' + dir
+        self.run(cmd, ssh_to_host=host)
+
+    def copy_to_remote_host(self, host, local_path, remote_path, mode=None):
+        # copy the local file to the remote host
+        ssh = SshConn(host)
+        self.make_remote_dir(host, remote_path, path_is_file=True)
+        ssh.copy_file(local_path, remote_path, mode)
 
     def write_to_remote_host(self, host, local_path, file_name):
-        # make sure the directory is there and has the right permissions
-        ssh = SshConn(host)
-        dir = self.PRODUCTION_CONFIG_DIR
-        ssh.exec('sudo mkdir -p ' + dir + ' && sudo chmod -R a+wr ' + dir)
-
-        # copy the local file to the remote host
-        remote_path = dir + '/' + file_name
-        ssh.copy_file(local_path, remote_path)
+        remote_path = local_path  # copy to config dir first
+        self.copy_to_remote_host(host, local_path, remote_path)
 
     def write_to_local_host(self, file_path, content):
         f = open(file_path, "w")
