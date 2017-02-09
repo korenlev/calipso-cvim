5c5ccae8870b2c2d9fba6511d22f0b417127946b
yayogev <yayogev@cisco.com>
2017-02-09 15:10:38 +0200

    take mongo config file from our app/config folder by default

--- app/discover/mongo_access.py
+++ app/discover/mongo_access.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from bson.objectid import ObjectId
 from pymongo import MongoClient
@@ -15,21 +16,30 @@ from discover.logger import Logger
 class MongoAccess(Logger):
     client = None
     db = None
-    default_conf_file = "/etc/osdna/mongo.conf"
+    default_conf_file = 'osdna_mongo_access.conf'
 
     def __init__(self, config_file=""):
         super().__init__()
         self.mongo_connect(config_file)
 
     def mongo_connect(self, config_file=""):
-        if (MongoAccess.client != None):
+        if (MongoAccess.client is not None):
             return
         self.connect_params = {
             "server": "localhost",
             "port": 27017
         }
-        if not config_file and os.path.isfile(self.default_conf_file):
-            config_file = self.default_conf_file
+        if not config_file:
+            # try to look in the current work directory
+            # as defined by PYTHONPATH
+            python_path = os.environ['PYTHONPATH']
+            if os.pathsep in python_path:
+                python_path = python_path.split(os.pathsep)[0]
+            config_file = python_path + '/config/' + self.default_conf_file
+            if not os.path.isfile(config_file):
+                msg = "failed to open config file: " + config_file
+                self.log.error(msg)
+                sys.exit(1)
         if config_file:
             # read connection parameters from file
             try:
@@ -45,7 +55,7 @@ class MongoAccess(Logger):
                         val = l[pos + 1:].strip()
                         if val:
                             self.connect_params[attr] = val
-            except Exception as e:
+            except Exception:
                 self.log.error("failed to open config file: " + config_file)
                 raise
         self.prepare_connect_uri()
@@ -83,10 +93,11 @@ class MongoAccess(Logger):
     # Convert a nested dictionary from one convention to another.
     # Args:
     #     d (dict): dictionary (nested or not) to be converted.
-    #     convert_function (func): function that takes the string in one convention and returns it in the other one.
+    #     cf (func): convert function - takes the string in one convention,
+    #                returns it in the other one.
     # Returns:
     #     Dictionary with the new keys.
-    def change_dict_naming_convention(self, d, convert_function):
+    def change_dict_naming_convention(self, d, cf):
         new = {}
         if not d:
             return d
@@ -97,10 +108,10 @@ class MongoAccess(Logger):
         for k, v in d.items():
             new_v = v
             if isinstance(v, dict):
-                new_v = self.change_dict_naming_convention(v, convert_function)
+                new_v = self.change_dict_naming_convention(v, cf)
             elif isinstance(v, list):
                 new_v = list()
                 for x in v:
-                    new_v.append(self.change_dict_naming_convention(x, convert_function))
-            new[convert_function(k)] = new_v
+                    new_v.append(self.change_dict_naming_convention(x, cf))
+            new[cf(k)] = new_v
         return new
