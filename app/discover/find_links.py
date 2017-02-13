9c8eb1dfe5329235ee700ef3c9a58eb1b07ecd59
korlev <korlev@cisco.com>
2017-02-13 10:18:32 +0200

    fix Python problem with mutable default argument {}

--- app/discover/find_links.py
+++ app/discover/find_links.py
@@ -11,9 +11,12 @@ class FindLinks(Fetcher):
     def create_link(self, env, host, source, source_id, target, target_id,
                     link_type, link_name, state, link_weight,
                     source_label="", target_label="",
-                    extra_attributes={}):
+                    extra_attributes=None):
+        if extra_attributes is None:
+            extra_attributes = {}
         link = self.inv.create_link(env, host,
                                     source, source_id, target, target_id,
-                                    link_type, link_name, state, link_weight)
+                                    link_type, link_name, state, link_weight,
+                                    extra_attributes)
         if self.monitoring_setup:
             self.monitoring_setup.create_setup(link)
