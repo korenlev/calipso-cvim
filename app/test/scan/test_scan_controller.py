526188317a74287bf2358dbd26c9e0d3e93de269
yayogev <yayogev@cisco.com>
2017-03-23 11:05:43 +0200

    fix PEP8 warnings, mostly line length

--- app/test/scan/test_scan_controller.py
+++ app/test/scan/test_scan_controller.py
@@ -1,10 +1,9 @@
 import sys
-import os
 
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 from test.scan.test_scan import TestScan
 from test.scan.test_data.scan import *
-from discover.scan import ScanController
+from discover.scan import ScanController, ScanPlan
 from test.scan import mock_module
 from test.scan.mock_module import ScanEnvironment
 
@@ -18,20 +17,33 @@ class TestScanController(TestScan):
 
     def check_args_values(self, args, arguments):
         self.assertEqual(args.mongo_config, arguments['MONGO_CONFIG'],
-                         "The value of mongo_config is wrong")
-        self.assertEqual(args.env, arguments['ENV'], "The value of environment is wrong")
-        self.assertEqual(args.type, arguments['TYPE'], "The value of type is wrong")
-        self.assertEqual(args.inventory, arguments['INVENTORY'], "The value of inventory si wrong")
-        self.assertEqual(args.scan_self, arguments['SCAN_SELF'], "The value of scan_self is wrong")
-        self.assertEqual(args.id, arguments['ID'], "The value of id is wrong")
-        self.assertEqual(args.parent_id, arguments['PARENT_ID'], 'The value of parent_id  is wrong')
-        self.assertEqual(args.parent_type, arguments['PARENT_TYPE'], "The value of parent_type is wrong")
-        self.assertEqual(args.id_field, arguments['ID_FIELD'], "The value of id_field is wrong")
-        self.assertEqual(args.loglevel, arguments['LOGLEVEL'], "The value of loglevel is wrong")
-        self.assertEqual(args.inventory_only, arguments['INVENTORY_ONLY'],"The value of inventory_only is wrong")
-        self.assertEqual(args.links_only, arguments['LINKS_ONLY'], "The value of links_only is wrong")
-        self.assertEqual(args.cliques_only, arguments['CLIQUES_ONLY'], "The value of cliques_only is wrong")
-        self.assertEqual(args.clear, arguments['CLEAR'], "The value of clear is wrong")
+                         'The value of mongo_config is wrong')
+        self.assertEqual(args.env, arguments['ENV'],
+                         'The value of environment is wrong')
+        self.assertEqual(args.type, arguments['TYPE'],
+                         'The value of type is wrong')
+        self.assertEqual(args.inventory, arguments['INVENTORY'],
+                         'The value of inventory is wrong')
+        self.assertEqual(args.scan_self, arguments['SCAN_SELF'],
+                         'The value of scan_self is wrong')
+        self.assertEqual(args.id, arguments['ID'],
+                         'The value of id is wrong')
+        self.assertEqual(args.parent_id, arguments['PARENT_ID'],
+                         'The value of parent_id  is wrong')
+        self.assertEqual(args.parent_type, arguments['PARENT_TYPE'],
+                         'The value of parent_type is wrong')
+        self.assertEqual(args.id_field, arguments['ID_FIELD'],
+                         'The value of id_field is wrong')
+        self.assertEqual(args.loglevel, arguments['LOGLEVEL'],
+                         'The value of loglevel is wrong')
+        self.assertEqual(args.inventory_only, arguments['INVENTORY_ONLY'],
+                         'The value of inventory_only is wrong')
+        self.assertEqual(args.links_only, arguments['LINKS_ONLY'],
+                         'The value of links_only is wrong')
+        self.assertEqual(args.cliques_only, arguments['CLIQUES_ONLY'],
+                         'The value of cliques_only is wrong')
+        self.assertEqual(args.clear, arguments['CLEAR'],
+                         'The value of clear is wrong')
 
     def test_get_args_with_default_arguments(self):
         sys.argv = DEFAULT_COMMAND_ARGS
@@ -57,17 +69,20 @@ class TestScanController(TestScan):
         else:
             return default
 
-    def check_plan_values(self, plan, scanner_class, module_file, obj_id,
+    def check_plan_values(self, plan, scanner_class, obj_id,
                           child_type, child_id):
-        self.assertEqual(plan['scanner_class'], scanner_class, "The scanner class is wrong")
-        self.assertEqual(plan['module_file'], module_file, "The module file is wrong")
-        self.assertEqual(plan['child_type'], child_type, "The child type is wrong")
-        self.assertEqual(plan['child_id'], child_id, "The child id is wrong")
-        self.assertEqual(plan['object_id'], obj_id, "The object is wrong")
+        self.assertEqual(plan.scanner_class, scanner_class,
+                         'The scanner class is wrong')
+        self.assertEqual(plan.child_type, child_type,
+                         'The child type is wrong')
+        self.assertEqual(plan.child_id, child_id,
+                         'The child id is wrong')
+        self.assertEqual(plan.object_id, obj_id, 'The object is wrong')
 
     def test_prepare_scan_plan(self):
-        plan = self.scan_controller.prepare_scan_plan(SCAN_ENV_PLAN_TO_BE_PREPARED)
-        self.check_plan_values(plan, SCANNER_CLASS_FOR_ENV, MODULE_FILE_FOR_ENV,
+        scan_plan = ScanPlan(SCAN_ENV_PLAN_TO_BE_PREPARED)
+        plan = self.scan_controller.prepare_scan_plan(scan_plan)
+        self.check_plan_values(plan, SCANNER_CLASS_FOR_ENV,
                                OBJ_ID_FOR_ENV, CHILD_TYPE_FOR_ENV,
                                CHILD_ID_FOR_ENV)
 
@@ -76,27 +91,35 @@ class TestScanController(TestScan):
         self.inv.get_by_id = MagicMock(return_value=REGIONS_FOLDER)
 
         self.scan_controller.inv = self.inv
-        plan = self.scan_controller.prepare_scan_plan(SCAN_REGION_PLAN_TO_BE_PREPARED)
+        scan_plan = ScanPlan(SCAN_REGION_PLAN_TO_BE_PREPARED)
+        plan = self.scan_controller.prepare_scan_plan(scan_plan)
 
-        self.check_plan_values(plan, SCANNER_CLASS_FOR_REGION, MODULE_FILE_FOR_REGION,
+        self.check_plan_values(plan, SCANNER_CLASS_FOR_REGION,
                                OBJ_ID_FOR_REGION, CHILD_TYPE_FOR_REGION,
                                CHILD_ID_FOR_REGION)
         self.inv.get_by_id = original_get_by_id
 
     def test_prepare_scan_region_folder_plan(self):
-        plan = self.scan_controller.prepare_scan_plan(SCAN_PROJECT_FOLDER_PLAN_TO_BE_PREPARED)
-        self.check_plan_values(plan, SCANNER_CLASS_FOR_REGION_FOLDER, MODULE_FILE_FOR_REGION_FOLDER,
-                               OBJ_ID_FOR_REGION_FOLDER, CHILD_TYPE_FOR_REGION_FOLDER,
+        scan_plan = ScanPlan(SCAN_PROJECT_FOLDER_PLAN_TO_BE_PREPARED)
+        plan = self.scan_controller.prepare_scan_plan(scan_plan)
+        self.check_plan_values(plan, SCANNER_CLASS_FOR_REGION_FOLDER,
+                               OBJ_ID_FOR_REGION_FOLDER,
+                               CHILD_TYPE_FOR_REGION_FOLDER,
                                CHILD_ID_FOR_REGION_FOLDER)
 
-    def check_scan_counts(self, run_scan_count, scan_cliques_count, scan_links_count):
-        self.assertEqual(ScanEnvironment.run_scan_count, run_scan_count, "run_scan count is wrong")
-        self.assertEqual(ScanEnvironment.scan_cliques_count, scan_cliques_count, "scan_cliques count is wrong")
-        self.assertEqual(ScanEnvironment.scan_links_count, scan_links_count, "scan_links count is wrong")
+    def check_scan_counts(self, run_scan_count, scan_cliques_count,
+                          scan_links_count):
+        self.assertEqual(ScanEnvironment.run_scan_count, run_scan_count,
+                         'run_scan count is wrong')
+        self.assertEqual(ScanEnvironment.scan_cliques_count, scan_cliques_count,
+                         'scan_cliques count is wrong')
+        self.assertEqual(ScanEnvironment.scan_links_count, scan_links_count,
+                         'scan_links count is wrong')
 
     def test_scan(self):
         self.scan_controller.get_args = MagicMock()
-        self.scan_controller.get_scan_plan = MagicMock(return_value=PREPARED_ENV_PLAN)
+        self.scan_controller.get_scan_plan = \
+            MagicMock(return_value=PREPARED_ENV_PLAN)
 
         self.scan_controller.run()
         self.check_scan_counts(1, 1, 1)
@@ -104,7 +127,8 @@ class TestScanController(TestScan):
 
     def test_scan_with_inventory_only(self):
         self.scan_controller.get_args = MagicMock()
-        self.scan_controller.get_scan_plan = MagicMock(return_value=PREPARED_ENV_INVENTORY_ONLY_PLAN)
+        self.scan_controller.get_scan_plan = \
+            MagicMock(return_value=PREPARED_ENV_INVENTORY_ONLY_PLAN)
 
         self.scan_controller.run()
         self.check_scan_counts(1, 0, 0)
@@ -112,7 +136,8 @@ class TestScanController(TestScan):
 
     def test_scan_with_links_only(self):
         self.scan_controller.get_args = MagicMock()
-        self.scan_controller.get_scan_plan = MagicMock(return_value=PREPARED_ENV_LINKS_ONLY_PLAN)
+        self.scan_controller.get_scan_plan = \
+            MagicMock(return_value=PREPARED_ENV_LINKS_ONLY_PLAN)
 
         self.scan_controller.run()
         self.check_scan_counts(0, 0, 1)
@@ -120,7 +145,8 @@ class TestScanController(TestScan):
 
     def test_scan_with_cliques_only(self):
         self.scan_controller.get_args = MagicMock()
-        self.scan_controller.get_scan_plan = MagicMock(return_value=PREPARED_ENV_CLIQUES_ONLY_PLAN)
+        self.scan_controller.get_scan_plan = \
+            MagicMock(return_value=PREPARED_ENV_CLIQUES_ONLY_PLAN)
 
         self.scan_controller.run()
         self.check_scan_counts(0, 1, 0)
