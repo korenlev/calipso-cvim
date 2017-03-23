991b9f0ccca49e30b47a7c2f09e0655787b62a2f
yaronyogev <yaronyogev@gmail.com>
2017-03-23 19:17:34 +0200

    check_plan_values - reversed order to match expected and actual in assertEqual need to use ScanPlan now with values to scanner check_args_values refactored to make it shorter and handle expected values being shorter removedd MODULE_* values - checking only scanner class fixed a lot of the test data

--- app/test/scan/test_scan_controller.py
+++ app/test/scan/test_scan_controller.py
@@ -13,37 +13,22 @@ class TestScanController(TestScan):
     def setUp(self):
         self.configure_environment()
         self.scan_controller = ScanController()
-        sys.modules[MODULE_NAME_FOR_IMPORT] = mock_module
 
-    def check_args_values(self, args, arguments):
-        self.assertEqual(args.mongo_config, arguments['MONGO_CONFIG'],
-                         'The value of mongo_config is wrong')
-        self.assertEqual(args.env, arguments['ENV'],
-                         'The value of environment is wrong')
-        self.assertEqual(args.type, arguments['TYPE'],
-                         'The value of type is wrong')
-        self.assertEqual(args.inventory, arguments['INVENTORY'],
-                         'The value of inventory is wrong')
-        self.assertEqual(args.scan_self, arguments['SCAN_SELF'],
-                         'The value of scan_self is wrong')
-        self.assertEqual(args.id, arguments['ID'],
-                         'The value of id is wrong')
-        self.assertEqual(args.parent_id, arguments['PARENT_ID'],
-                         'The value of parent_id  is wrong')
-        self.assertEqual(args.parent_type, arguments['PARENT_TYPE'],
-                         'The value of parent_type is wrong')
-        self.assertEqual(args.id_field, arguments['ID_FIELD'],
-                         'The value of id_field is wrong')
-        self.assertEqual(args.loglevel, arguments['LOGLEVEL'],
-                         'The value of loglevel is wrong')
-        self.assertEqual(args.inventory_only, arguments['INVENTORY_ONLY'],
-                         'The value of inventory_only is wrong')
-        self.assertEqual(args.links_only, arguments['LINKS_ONLY'],
-                         'The value of links_only is wrong')
-        self.assertEqual(args.cliques_only, arguments['CLIQUES_ONLY'],
-                         'The value of cliques_only is wrong')
-        self.assertEqual(args.clear, arguments['CLEAR'],
-                         'The value of clear is wrong')
+    def arg_validate(self, args, expected, key, err=None):
+        if key not in expected:
+            return
+        err = err if err else 'The value of {} is wrong'.format(key)
+        self.assertEqual(args.get(key, None), expected[key.upper()], err)
+
+    def check_args_values(self, args, expected):
+        self.arg_validate(args, expected, 'env',
+                          'The value of environment is wrong')
+        keys = ['mongo_config', 'mongo_config', 'type', 'inventory',
+                'scan_self', 'id', 'parent_id', 'parent_type', 'id_field',
+                'loglevel', 'inventory_only', 'links_only', 'cliques_only',
+                'clear']
+        for key in keys:
+            self.arg_validate(args, expected, key)
 
     def test_get_args_with_default_arguments(self):
         sys.argv = DEFAULT_COMMAND_ARGS
@@ -55,13 +40,37 @@ class TestScanController(TestScan):
         sys.argv = SHORT_COMMAND_ARGS
         args = self.scan_controller.get_args()
         # check the value parsed by short arguments
-        self.check_args_values(args, ARGUMENTS)
+        self.check_args_values(args, SHORT_FLAGS_ARGUMENTS)
 
     def test_get_args_with_full_command_args(self):
         sys.argv = LONG_COMMAND_ARGS
         args = self.scan_controller.get_args()
         # check the value parsed by long arguments
-        self.check_args_values(args, ARGUMENTS)
+        self.check_args_values(args, ARGUMENTS_FULL)
+
+    def test_get_args_with_full_command_args_clear_all(self):
+        sys.argv = LONG_COMMAND_ARGS_CLEAR_ALL
+        args = self.scan_controller.get_args()
+        # check the value parsed by long arguments
+        self.check_args_values(args, ARGUMENTS_FULL_CLEAR_ALL)
+
+    def test_get_args_with_full_command_args_inventory_only(self):
+        sys.argv = LONG_COMMAND_ARGS_INVENTORY_ONLY
+        args = self.scan_controller.get_args()
+        # check the value parsed by long arguments
+        self.check_args_values(args, ARGUMENTS_FULL_INVENTORY_ONLY)
+
+    def test_get_args_with_full_command_args_links_only(self):
+        sys.argv = LONG_COMMAND_ARGS_LINKS_ONLY
+        args = self.scan_controller.get_args()
+        # check the value parsed by long arguments
+        self.check_args_values(args, ARGUMENTS_FULL_LINKS_ONLY)
+
+    def test_get_args_with_full_command_args_cliques_only(self):
+        sys.argv = LONG_COMMAND_ARGS_CLIQUES_ONLY
+        args = self.scan_controller.get_args()
+        # check the value parsed by long arguments
+        self.check_args_values(args, ARGUMENTS_FULL_CLIQUES_ONLY)
 
     def side_effect(self, key, default):
         if key in FORM.keys():
@@ -71,13 +80,13 @@ class TestScanController(TestScan):
 
     def check_plan_values(self, plan, scanner_class, obj_id,
                           child_type, child_id):
-        self.assertEqual(plan.scanner_class, scanner_class,
+        self.assertEqual(scanner_class, plan.scanner_class,
                          'The scanner class is wrong')
-        self.assertEqual(plan.child_type, child_type,
+        self.assertEqual(child_type, plan.child_type,
                          'The child type is wrong')
-        self.assertEqual(plan.child_id, child_id,
+        self.assertEqual(child_id, plan.child_id,
                          'The child id is wrong')
-        self.assertEqual(plan.object_id, obj_id, 'The object is wrong')
+        self.assertEqual(obj_id, plan.object_id, 'The object is wrong')
 
     def test_prepare_scan_plan(self):
         scan_plan = ScanPlan(SCAN_ENV_PLAN_TO_BE_PREPARED)
@@ -100,7 +109,7 @@ class TestScanController(TestScan):
         self.inv.get_by_id = original_get_by_id
 
     def test_prepare_scan_region_folder_plan(self):
-        scan_plan = ScanPlan(SCAN_PROJECT_FOLDER_PLAN_TO_BE_PREPARED)
+        scan_plan = ScanPlan(SCAN_REGION_FOLDER_PLAN_TO_BE_PREPARED)
         plan = self.scan_controller.prepare_scan_plan(scan_plan)
         self.check_plan_values(plan, SCANNER_CLASS_FOR_REGION_FOLDER,
                                OBJ_ID_FOR_REGION_FOLDER,
@@ -128,7 +137,7 @@ class TestScanController(TestScan):
     def test_scan_with_inventory_only(self):
         self.scan_controller.get_args = MagicMock()
         self.scan_controller.get_scan_plan = \
-            MagicMock(return_value=PREPARED_ENV_INVENTORY_ONLY_PLAN)
+            MagicMock(return_value=ScanPlan(PREPARED_ENV_INVENTORY_ONLY_PLAN))
 
         self.scan_controller.run()
         self.check_scan_counts(1, 0, 0)
@@ -137,7 +146,7 @@ class TestScanController(TestScan):
     def test_scan_with_links_only(self):
         self.scan_controller.get_args = MagicMock()
         self.scan_controller.get_scan_plan = \
-            MagicMock(return_value=PREPARED_ENV_LINKS_ONLY_PLAN)
+            MagicMock(return_value=ScanPlan(PREPARED_ENV_LINKS_ONLY_PLAN))
 
         self.scan_controller.run()
         self.check_scan_counts(0, 0, 1)
@@ -145,12 +154,11 @@ class TestScanController(TestScan):
 
     def test_scan_with_cliques_only(self):
         self.scan_controller.get_args = MagicMock()
-        self.scan_controller.get_scan_plan = \
-            MagicMock(return_value=PREPARED_ENV_CLIQUES_ONLY_PLAN)
+        scan_plan = ScanPlan(PREPARED_ENV_CLIQUES_ONLY_PLAN)
+        plan = self.scan_controller.prepare_scan_plan(scan_plan)
+        self.scan_controller.get_scan_plan = MagicMock(return_value=plan)
 
         self.scan_controller.run()
         self.check_scan_counts(0, 1, 0)
         ScanEnvironment.reset_counts()
 
-    def tearDown(self):
-        del sys.modules[MODULE_NAME_FOR_IMPORT]
