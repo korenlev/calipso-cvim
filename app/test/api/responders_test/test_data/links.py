7937919369f53ef35e2ffe4fff6526b3dc61f647
xiaocdon <xiaocdon@cisco.com>
2017-03-28 15:34:22 -0700

    Added unit test for links query

---
+++ app/test/api/responders_test/test_data/links.py
@@ -0,0 +1,18 @@
+URL = "/links"
+
+UNKNOWN_HOST = "unknown host"
+LINKS = [{
+    "id": "58ca73ae3a8a836d10ff3b45",
+    "host": "node-1.cisco.com",
+    "link_type": "pnic-network",
+    "link_name": "Segment-103",
+    "environment": "Mirantis-Liberty"}
+]
+
+LINK_ID = "58ca73ae3a8a836d10ff3b45"
+WRONG_LINK_ID = "58ca73ae3a8a836d10ff3b4"
+NONEXISTENT_LINK_ID = "58ca73ae3a8a836d10ff3b46"
+
+LINKS_LIST_RESPONSE = {
+    "links": LINKS
+}
