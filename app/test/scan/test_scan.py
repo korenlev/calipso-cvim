7a6d6f722011b08f92cef5176713c67d71402c1a
yaronyogev <yaronyogev@gmail.com>
2017-04-06 17:06:29 +0300

    refactor testing of scan for folder objects

--- app/test/scan/test_scan.py
+++ app/test/scan/test_scan.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import MagicMock
 
 from discover.configuration import Configuration
+from discover.scanner import Scanner
 from monitoring.setup.monitoring_setup_manager import MonitoringSetupManager
 from test.scan.config.local_config \
     import MONGODB_CONFIG, ENV_CONFIG, COLLECTION_CONFIG
@@ -24,3 +25,14 @@ class TestScan(unittest.TestCase):
         self.inv.monitoring_setup_manager = \
             MonitoringSetupManager(MONGODB_CONFIG, self.env)
         MonitoringSetupManager.create_setup = MagicMock()
+
+    def test_folder_object(self, scanner, expected_queue):
+        scanner.set_env(self.env)
+        Scanner.scan_from_queue = MagicMock()
+        scanner.run_scan({'id': 'test-env'}, 'id', None, None)
+        queue = scanner.scan_queue_track
+        expected_len = len(expected_queue)
+        self.assertEqual(len(queue), expected_len,
+                         'expected {} results from ScanEnvironment'.
+                         format(expected_len))
+        self.assertListEqual(list(queue.keys()), expected_queue)
--- app/test/scan/test_scan_environment.py
+++ app/test/scan/test_scan_environment.py
@@ -1,37 +1,16 @@
-from unittest.mock import MagicMock
-
 from discover.scan_environment import ScanEnvironment
-from discover.scanner import Scanner
 from test.scan.test_scan import TestScan
 
 
 class TestScanEnvironment(TestScan):
 
     ENV = 'test-env'
-    EXPECTED_RESULTS = [
-        {
-            "type": "regions_folder",
-            "children_scanner": "ScanRegionsRoot"
-        },
-        {
-            "type": "projects_folder",
-            "children_scanner": "ScanProjectsRoot"
-        }
-    ]
 
     def setUp(self):
         self.configure_environment()
 
     def test_scan_environment(self):
-        scanner = ScanEnvironment()
-        scanner.set_env(self.env)
-        Scanner.scan_from_queue = MagicMock()
-        scanner.run_scan({'id': 'test-env'}, 'id', None, None)
-        queue = scanner.scan_queue_track
-        self.assertEqual(len(queue), 2,
-                         'expected 2 results from ScanEnvironment')
-        self.assertListEqual(list(queue.keys()), [
+        self.test_folder_object(ScanEnvironment(), [
             'regions_folder;{}-regions'.format(self.ENV),
             'projects_folder;{}-projects'.format(self.ENV)
         ])
-
