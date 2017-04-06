a241aa1e2ff38ad30cd4d19b483ac8d8ea40115c
yaronyogev <yaronyogev@gmail.com>
2017-04-06 16:06:42 +0300

    fix completely flawed testing of ScanEnvironment

--- app/test/scan/test_scan_environment.py
+++ app/test/scan/test_scan_environment.py
@@ -1,30 +1,37 @@
-from copy import deepcopy
+from unittest.mock import MagicMock
 
 from discover.scan_environment import ScanEnvironment
-
+from discover.scanner import Scanner
 from test.scan.test_scan import TestScan
 
 
 class TestScanEnvironment(TestScan):
+
+    ENV = 'test-env'
+    EXPECTED_RESULTS = [
+        {
+            "type": "regions_folder",
+            "children_scanner": "ScanRegionsRoot"
+        },
+        {
+            "type": "projects_folder",
+            "children_scanner": "ScanProjectsRoot"
+        }
+    ]
+
     def setUp(self):
         self.configure_environment()
 
-    def test_scan_env_for_environment_condition(self):
-
-        self.scan_env = ScanEnvironment()
-        self.scan_env.set_env(self.env)
-
-        types_to_fetch = deepcopy(self.scan_env.types_to_fetch)
-
-        # Check env type for regions_folder
-        type_for_region_folder = self.scan_env.check_type_env(types_to_fetch[0])
-
-        # types to fetch don't have environment_condition hence it will return True
-        self.assertEqual(type_for_region_folder, True)
-
-        # Check env type for projects_folder
-        type_for_project_folder = self.scan_env.check_type_env(types_to_fetch[1])
-
-        # types to fetch don't have environment_condition hence it will return True
-        self.assertEqual(type_for_project_folder, True)
+    def test_scan_environment(self):
+        scanner = ScanEnvironment()
+        scanner.set_env(self.env)
+        Scanner.scan_from_queue = MagicMock()
+        scanner.run_scan({'id': 'test-env'}, 'id', None, None)
+        queue = scanner.scan_queue_track
+        self.assertEqual(len(queue), 2,
+                         'expected 2 results from ScanEnvironment')
+        self.assertListEqual(list(queue.keys()), [
+            'regions_folder;{}-regions'.format(self.ENV),
+            'projects_folder;{}-projects'.format(self.ENV)
+        ])
 
