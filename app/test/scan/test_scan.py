bd0937ae429112ad95e83741b06759667890c39c
yaronyogev <yaronyogev@gmail.com>
2017-04-06 16:05:11 +0300

    mock MonitoringSetupManager.create_setup set self.inv.monitoring_setup_manager

--- app/test/scan/test_scan.py
+++ app/test/scan/test_scan.py
@@ -1,6 +1,8 @@
 import unittest
+from unittest.mock import MagicMock
 
 from discover.configuration import Configuration
+from monitoring.setup.monitoring_setup_manager import MonitoringSetupManager
 from test.scan.config.local_config \
     import MONGODB_CONFIG, ENV_CONFIG, COLLECTION_CONFIG
 from test.scan.test_data.configurations import CONFIGURATIONS
@@ -19,3 +21,6 @@ class TestScan(unittest.TestCase):
         self.inv = InventoryMgr()
         self.conf.env_config = CONFIGURATIONS
         self.inv.set_collections(self.inventory_collection)
+        self.inv.monitoring_setup_manager = \
+            MonitoringSetupManager(MONGODB_CONFIG, self.env)
+        MonitoringSetupManager.create_setup = MagicMock()
