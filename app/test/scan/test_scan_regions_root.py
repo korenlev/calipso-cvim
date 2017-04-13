7b862e36c4d9aae5716c82da0c430724b2d8cccc
yaronyogev <yaronyogev@gmail.com>
2017-04-13 10:26:36 +0300

    test scan of regions root by scan queue contents

--- app/test/scan/test_scan_regions_root.py
+++ app/test/scan/test_scan_regions_root.py
@@ -1,21 +1,64 @@
-from copy import deepcopy
+from unittest.mock import MagicMock
 
 from discover.scan_regions_root import ScanRegionsRoot
-
+from discover.api_fetch_regions import ApiFetchRegions
+from discover.scanner import Scanner
+from test.scan.test_data.scan import UNIT_TESTS_ENV
 from test.scan.test_scan import TestScan
 
 
 class TestScanRegionsRoot(TestScan):
+
+    API_REGIONS_RESULT = [
+        {
+            'id': 'RegionOne',
+            'endpoints': {
+                'keystone': {
+                    'internalURL': 'http://192.168.0.2:5000/v2.0',
+                    'adminURL': 'http://192.168.0.2:35357/v2.0',
+                    'service_type': 'identity',
+                    'publicURL': 'http://172.16.0.3:5000/v2.0',
+                    'id': '18d3a7fc5a7f4240aa0505956d8b5591'},
+                'neutron': {
+                    'internalURL': 'http://192.168.0.2:9696',
+                    'adminURL': 'http://192.168.0.2:9696',
+                    'service_type': 'network',
+                    'publicURL': 'http://172.16.0.3:9696',
+                    'id': '68f4a43b3ac149148cd485fbcac4565f'},
+                'nova': {
+                    'internalURL': 'http://192.168.0.2:8774/v2/' +
+                                   'c769218f0d3c429a8585696cba3d9f5a',
+                    'adminURL': 'http://192.168.0.2:8774/v2/' +
+                                'c769218f0d3c429a8585696cba3d9f5a',
+                    'service_type': 'compute',
+                    'publicURL': 'http://172.16.0.3:8774/v2/' +
+                                 'c769218f0d3c429a8585696cba3d9f5a',
+                    'id': '0ce6d475ca8742fab6a846f620ae901f'}
+            },
+            'name': 'RegionOne',
+            'parent_id': UNIT_TESTS_ENV + '-regions',
+            'parent_type': 'regions_folder'
+        }
+    ]
+
     def setUp(self):
         self.configure_environment()
 
-    def test_scan_reg_root_type_for_env_condition(self):
+    def test_scan_regions_root(self):
 
-        self.scan_reg_root = ScanRegionsRoot()
-        self.scan_reg_root.set_env(self.env)
-
-        types_to_fetch = deepcopy(self.scan_reg_root.types_to_fetch)
-
-        result = self.scan_reg_root.check_type_env(types_to_fetch[0])
-
-        self.assertEqual(result, True)
+        scanner = ScanRegionsRoot()
+        scanner.set_env(UNIT_TESTS_ENV)
+        Scanner.scan_from_queue = MagicMock()
+        ApiFetchRegions.get = MagicMock(return_value=self.API_REGIONS_RESULT)
+        regions_folder = {
+            'id': UNIT_TESTS_ENV + '-regions',
+            'type': 'regions_folder',
+            'parent_type': 'environment',
+            'parent_id': UNIT_TESTS_ENV
+        }
+        scanner.scan(regions_folder)
+        queue = scanner.scan_queue_track
+        self.assertEqual(len(queue), 1,
+                         'expected 1 result from ScanRegionsRoot')
+        self.assertListEqual(list(queue.keys()), ['region;RegionOne'])
+        self.inv.set.assert_called_once()
