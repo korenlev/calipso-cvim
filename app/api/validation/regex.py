54d3c196889ec0b402ccb4f1d550634c0a0d14a7
xiaocdon <xiaocdon@cisco.com>
2017-04-24 11:00:51 -0700

    Compiled regular expressions for re-use

--- app/api/validation/regex.py
+++ app/api/validation/regex.py
@@ -5,17 +5,17 @@ IP = "ipv4/ipv6 address"
 HOSTNAME = "host name"
 PATH = "path"
 
-_PORT_REGEX = r'^0*(?:6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|' \
-           r'6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9])$'
+_PORT_REGEX = re.compile('^0*(?:6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|'
+                         '6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9])$')
 
-_HOSTNAME_REGEX = r'^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])' \
-               r'(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*$'
+_HOSTNAME_REGEX = re.compile('^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])'
+                             '(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*$')
 
-_PATH_REGEX = r'^(\/){1}([^\/\0]+(\/)?)+$'
+_PATH_REGEX = re.compile('^(\/){1}([^\/\0]+(\/)?)+$')
 
-_IPV4_REGEX = r'^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$'
-_IPV6_REGEX = r'^(((?=.*(::))(?!.*\3.+\3))\3?|[\dA-F]{1,4}:)([\dA-F]{1,4}(\3|:\b)|\2){5}' \
-             r'(([\dA-F]{1,4}(\3|:\b|$)|\2){2}|(((2[0-4]|1\d|[1-9])?\d|25[0-5])\.?\b){4})$'
+_IPV4_REGEX = re.compile('^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$')
+_IPV6_REGEX = re.compile('^(((?=.*(::))(?!.*\3.+\3))\3?|[\dA-F]{1,4}:)([\dA-F]{1,4}(\3|:\b)|\2){5}(([\dA-F]{1,4}'
+                         '(\3|:\b|$)|\2){2}|(((2[0-4]|1\d|[1-9])?\d|25[0-5])\.?\b){4})$')
 
 _REGEX_MAP = {
     PORT: _PORT_REGEX,
@@ -38,7 +38,7 @@ def validate(key, value, regex_names, error_message=None):
         try:
             value = str(value)
             match_regexes = [regex for regex in regexes
-                             if re.match(regex, value)]
+                             if regex.match(value)]
             if match_regexes:
                 return None
         except:
