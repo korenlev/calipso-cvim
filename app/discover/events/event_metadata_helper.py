7a32e1535173d6f4c9be10b8bae11c5b90fa98ea
yaronyogev <yaronyogev@gmail.com>
2017-05-08 19:11:38 +0300

    refactor common functionality from EventMetaDataParser to MetadataParser

--- app/discover/events/event_metadata_helper.py
+++ app/discover/events/event_metadata_helper.py
@@ -1,45 +1,44 @@
-import json
 from typing import List, Tuple
 
-import os
-
-from utils.util import get_extension
+from utils.metadata_parser import MetadataParser
 
 
-class EventMetadataParser:
+class EventMetadataParser(MetadataParser):
 
     HANDLERS_PACKAGE = 'handlers_package'
     QUEUES = 'queues'
     EVENT_HANDLERS = 'event_handlers'
 
-    REQUIRED_EXPORTS = (HANDLERS_PACKAGE, EVENT_HANDLERS)
+    REQUIRED_EXPORTS = [HANDLERS_PACKAGE, EVENT_HANDLERS]
 
     def __init__(self):
+        super().__init__()
         self.handlers_package = None
         self.queues = []
         self.event_handlers = []
-        self.errors = []
 
-    def _validate_required_fields(self, handlers_package, event_handlers):
-        if not handlers_package \
-           or not isinstance(handlers_package, str):
-            self.errors.append("Handlers package '{}' is invalid"
-                               .format(handlers_package))
+    def get_required_fields(self) -> list:
+        return self.REQUIRED_EXPORTS
 
-        if not event_handlers \
-           or not isinstance(event_handlers, dict):
-            self.errors.append("Event handlers variable is invalid or empty"
+    def validate_metadata(self, metadata: dict) -> bool:
+        super().validate_metadata(metadata)
+
+        package = metadata.get(self.HANDLERS_PACKAGE)
+        if not package or not isinstance(package, str):
+            self.errors.append("Handlers package '{}' is invalid".
+                               format(package))
+
+        event_handlers = metadata.get(self.EVENT_HANDLERS)
+        if not event_handlers or not isinstance(event_handlers, dict):
+            self.errors.append("Event handlers attribute is invalid or empty"
                                "(should be a non-empty dict)")
 
         return len(self.errors) == 0
 
-    def _finalize_parsing(self,
-                          handlers_package: str,
-                          queues: List[dict],
-                          event_handlers: dict):
-        # We shouldn't continue parsing if metadata file has any errors
-        if not self._validate_required_fields(handlers_package, event_handlers):
-            return
+    def _finalize_parsing(self, metadata):
+        handlers_package = metadata[self.HANDLERS_PACKAGE]
+        queues = metadata.get(self.QUEUES, None)
+        event_handlers = metadata[self.EVENT_HANDLERS]
 
         # Convert variables to EventHandler-friendly format
         self.handlers_package = handlers_package
@@ -55,41 +54,13 @@ class EventMetadataParser:
 
         self.event_handlers = event_handlers
 
-    def _parse_json_file(self, file_path: str):
-        with open(file_path) as data_file:
-            metadata = json.load(data_file)
-
-        # make sure metadata json contains all fields we need
-        if not all([field in metadata for field in self.REQUIRED_EXPORTS]):
-            self.errors.append("Metadata json should contain all the following fields: {}"
-                               .format(', '.join(self.REQUIRED_EXPORTS)))
-            return
-
-        handlers_package = metadata[self.HANDLERS_PACKAGE]
-        queues = metadata.get(self.QUEUES, None)
-        event_handlers = metadata[self.EVENT_HANDLERS]
-
-        self._finalize_parsing(handlers_package, queues, event_handlers)
-
-    def parse_metadata_file(self, file_path: str):
-        extension = get_extension(file_path)
-        if extension != 'json':
-            raise ValueError("Extension '{}' is not supported. "
-                             "Please provide a .json metadata file."
-                             .format(extension))
-
-        if not os.path.isfile(file_path):
-            raise ValueError("Couldn't load metadata file. Path '{}' doesn't exist or is not a file"
-                             .format(file_path))
-
-        # Try to parse metadata file if it has one of the supported extensions
-        self._parse_json_file(file_path)
-        if self.errors:
-            raise TypeError("Errors encountered during metadata file parsing:\n{}"
-                            .format("\n".join(self.errors)))
+    def parse_metadata_file(self, file_path: str) -> dict:
+        metadata = super().parse_metadata_file(file_path)
+        self._finalize_parsing(metadata)
+        return metadata
 
 
 def parse_metadata_file(file_path: str) -> Tuple[str, List[dict], dict]:
     parser = EventMetadataParser()
     parser.parse_metadata_file(file_path)
-    return parser.handlers_package, parser.queues, parser.event_handlers
+    return Tuple[parser.handlers_package, parser.queues, parser.event_handlers]
