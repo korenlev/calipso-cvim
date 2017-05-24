a8432dd31fcaf116de856d32f145fecc20056fde
Yaron Yogev <yayogev@cisco.com>
2017-05-24 17:53:04 +0300

    US2384 Support SSL use by Sensu RabbitMQ transport

--- app/monitoring/setup/monitoring_host.py
+++ app/monitoring/setup/monitoring_host.py
@@ -1,7 +1,15 @@
 import copy
-from os.path import join
+import os
+from os.path import join, sep
 
 from monitoring.setup.monitoring_handler import MonitoringHandler
+
+RABBITMQ_CONFIG_FILE = 'rabbitmq.json'
+RABBITMQ_CONFIG_ATTR = 'rabbitmq'
+
+RABBITMQ_CERT_FILE_ATTR = 'cert_chain_file'
+RABBITMQ_PK_FILE_ATTR = 'private_key_file'
+TMP_FILES_DIR = '/tmp'
 
 
 class MonitoringHost(MonitoringHandler):
@@ -34,6 +42,7 @@ class MonitoringHost(MonitoringHandler):
         # copy configuration files
         for file_name in sensu_host_files:
             content = self.prepare_config_file(file_name, {'side': 'client'})
+            self.get_ssl_files(host_id, file_name, content)
             self.write_config_file(file_name, sub_dir, host_id, content)
 
         if self.provision < self.provision_levels['deploy']:
@@ -43,3 +52,31 @@ class MonitoringHost(MonitoringHandler):
 
         # mark this environment as prepared
         self.configuration.update_env({'monitoring_setup_done': True})
+
+    def get_ssl_files(self, host, file_type, content):
+        if self.fetch_ssl_files:
+            return  # already got names of SSL files
+        if file_type != RABBITMQ_CONFIG_FILE:
+            return
+        if not isinstance(content, dict):
+            self.log.warn('invalid content of {}'.format(RABBITMQ_CONFIG_FILE))
+            return
+        config = content['config']
+        if not config:
+            self.log.warn('invalid content of {}'.format(RABBITMQ_CONFIG_FILE))
+            return
+        if RABBITMQ_CONFIG_ATTR not in config:
+            self.log.warn('invalid content of {}'.format(RABBITMQ_CONFIG_FILE))
+            return
+        ssl_conf = config.get(RABBITMQ_CONFIG_ATTR).get('ssl')
+        if not ssl_conf:
+            return  # SSL not used
+
+        for path_attr in [RABBITMQ_CERT_FILE_ATTR, RABBITMQ_PK_FILE_ATTR]:
+            path = ssl_conf.get(path_attr)
+            if not path:
+                self.log.error('missing SSL path {}'.format(path_attr))
+                return
+            # this configuration requires SSL
+            # keep the path of the files for later use
+            self.fetch_ssl_files.append(path)
