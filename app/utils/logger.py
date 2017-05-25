864b0399347a075ebe502c04cb48b52c09dd0ae9
Yaron Yogev <yayogev@cisco.com>
2017-05-25 19:58:30 +0300

    US2585 write log to MongoDB currently not running due to - we get error "unable to import Configuration"   when importing inventory_mgr.py

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,10 +1,50 @@
+import datetime
 import logging
 
+from messages.message import Message
+from utils.inventory_mgr import InventoryMgr
 
-class Logger:
+SOURCE_SYSTEM = 'CALIPSO'
+
+
+class MongoLoggingHandler(logging.Handler):
+    """
+    Logging handler for MongoDB
+    """
+
+    def __init__(self, _env, _level):
+        super().__init__(_level)
+        self.level = _level
+        self.env = _env
+        self.inv = InventoryMgr()
+
+    def emit(self, record):
+        # make sure we do not try to log to DB when DB is not ready
+        if not self.inv.is_db_ready():
+            return
+        # make ID from current timestamp
+        d = datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)
+        timestamp_id = '{}.{}.{}'.format(d.days, d.seconds, d.microseconds)
+        source = SOURCE_SYSTEM
+        object_id = ''
+        display_context = ''
+        message = Message(timestamp_id, self.env, source,
+                          object_id, display_context,
+                          msg=Logger.formatter.format(record),
+                          level=self.level)
+        self.inv.collections['messages'].insert_one(message.get())
+
+
+class Logger(object):
+    formatter = None
+
     def __init__(self):
         super().__init__()
         self.log = logging.getLogger("OS-DNA")
+        self.env = None
+
+    def set_env(self, _env):
+        self.env = _env
 
     def set_loglevel(self, loglevel):
         # assuming loglevel is bound to the string value obtained from the
@@ -16,11 +56,15 @@ class Logger:
         logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
                             level=numeric_level)
         logger = logging.getLogger("OS-DNA")
-        formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+        Logger.formatter = \
+            logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
         ch = logging.StreamHandler()
         ch.setLevel(numeric_level)
-        ch.setFormatter(formatter)
-        logger.addHandler(ch)
+        ch.setFormatter(Logger.formatter)
+        # assuming level was set previously if there already is a handler
         logger.propagate = False
+        if not logger.hasHandlers():
+            logger.addHandler(ch)
+            # logger.addHandler(MongoLoggingHandler(numeric_level, self.env))
         logger.setLevel(numeric_level)
         self.log = logger
