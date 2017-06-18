180387189e9d1f9e7f825b16f643e9b2dae2ff98
Eyal Lapid <eyal.lapid@protonmail.com>
2017-06-18 11:58:52 +0300

    add logs

--- imports/api/supported_environments/supported_environments.js
+++ imports/api/supported_environments/supported_environments.js
@@ -1,4 +1,5 @@
 import { Mongo } from 'meteor/mongo';
+import * as R from 'ramda';
 
 export const SupportedEnvironments = new Mongo.Collection(
   'supported_environments', { idGeneration: 'MONGO' });
@@ -6,19 +7,35 @@ export const SupportedEnvironments = new Mongo.Collection(
 export const subsNameSupportedEnvs = 'supported-environments';
 
 export function isMonitoringSupported(distribution, type_drivers, mechanism_drivers) {
-  return SupportedEnvironments.find({
+  console.log('isMonitoringSupported');
+  console.log(`distribution: ${R.toString(distribution)}`);
+  console.log(`type_drivers: ${R.toString(type_drivers)}`);
+  console.log(`mechanism_drivers: ${R.toString(mechanism_drivers)}`);
+
+  let result = SupportedEnvironments.find({
     'environment.distribution': distribution,
     'environment.type_drivers': type_drivers,
     'environment.mechanism_drivers': { $in: mechanism_drivers },
     'features.monitoring': true
   }).count() > 0;
+
+  console.log(`result: ${R.toString(result)}`);
+  return result;
 }
 
 export function isListeningSupported(distribution, type_drivers, mechanism_drivers) {
-  return SupportedEnvironments.find({
+  console.log('isListeningSupported');
+  console.log(`distribution: ${R.toString(distribution)}`);
+  console.log(`type_drivers: ${R.toString(type_drivers)}`);
+  console.log(`mechanism_drivers: ${R.toString(mechanism_drivers)}`);
+
+  let result = SupportedEnvironments.find({
     'environment.distribution': distribution,
     'environment.type_drivers': type_drivers,
     'environment.mechanism_drivers': { $in: mechanism_drivers },
     'features.listening': true
   }).count() > 0;
+
+  console.log(`result: ${R.toString(result)}`);
+  return result;
 }
