7aeda54482db1f2921597def1bb5e4bc927b4cd4
Yaron Yogev <yayogev@cisco.com>
2017-06-19 18:21:54 +0300

    US1049 added service_type to parameters of check_vservice.py

--- app/monitoring/setup/monitoring_vservice.py
+++ app/monitoring/setup/monitoring_vservice.py
@@ -8,4 +8,7 @@ class MonitoringVservice(MonitoringSimpleObject):
 
     def create_setup(self, o):
         self.setup('vservice', o,
-                   values={'local_service_id': o['local_service_id']})
+                   values={
+                       'local_service_id': o['local_service_id'],
+                       'service_type': o['service_type']
+                   })
