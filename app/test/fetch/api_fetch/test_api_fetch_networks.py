4a099231c30bc71ba43ceeedd425c7858f17f929
xiaocdon <xiaocdon@cisco.com>
2017-06-26 11:17:54 -0700

    rename all get_for_region to get_networks, since the get_for_region method has been renamed to get_networks

--- app/test/fetch/api_fetch/test_api_fetch_networks.py
+++ app/test/fetch/api_fetch/test_api_fetch_networks.py
@@ -13,7 +13,7 @@ class TestApiFetchNetworks(TestFetch):
         self.fetcher = ApiFetchNetworks()
         self.set_regions_for_fetcher(self.fetcher)
 
-    def test_get_for_region(self):
+    def test_get_networks(self):
         # mock endpoint
         self.fetcher.get_region_url_nover = MagicMock(return_value=ENDPOINT)
         # mock networks response and subnet response
@@ -21,20 +21,20 @@ class TestApiFetchNetworks(TestFetch):
         # mock project info
         self.fetcher.inv.get_by_id = MagicMock(return_value=PROJECT)
 
-        result = self.fetcher.get_for_region(REGION_NAME, TOKEN)
+        result = self.fetcher.get_networks(REGION_NAME, TOKEN)
         self.assertNotEqual(result, [], "Can't get networks info")
         self.assertIn("project", result[0], "Can't put project info in the network doc")
 
-    def test_get_for_region_with_wrong_networks_response(self):
+    def test_get_networks_with_wrong_networks_response(self):
         # mock endpoint
         self.fetcher.get_region_url_nover = MagicMock(return_value=ENDPOINT)
         # mock the wrong networks response
         self.fetcher.get_url = MagicMock(return_value=WRONG_NETWORK_RESPONSE)
 
-        result = self.fetcher.get_for_region(REGION_NAME, TOKEN)
+        result = self.fetcher.get_networks(REGION_NAME, TOKEN)
         self.assertEqual(result, [], "Can't get empty empty when the networks response is wrong")
 
-    def test_get_for_region_with_wrong_subnet_response(self):
+    def test_get_networks_with_wrong_subnet_response(self):
         # mock endpoint
         self.fetcher.get_region_url_nover = MagicMock(return_value=ENDPOINT)
         # mock the correct networks response and wrong subnets response
@@ -42,20 +42,20 @@ class TestApiFetchNetworks(TestFetch):
         # mock project info
         self.fetcher.inv.get_by_id = MagicMock(return_value=PROJECT)
 
-        result = self.fetcher.get_for_region(REGION_NAME, TOKEN)
+        result = self.fetcher.get_networks(REGION_NAME, TOKEN)
 
         self.assertNotEqual(result, [], "Can't get networks info when the subnets response is wrong")
 
     def test_get(self):
-        # store original get_for_region method
-        original_method = self.fetcher.get_for_region
-        # mock the get_for_region method
-        self.fetcher.get_for_region = MagicMock(return_value=GET_FOR_REGION)
+        # store original get_networks method
+        original_method = self.fetcher.get_networks
+        # mock the get_networks method
+        self.fetcher.get_networks = MagicMock(return_value=GET_FOR_REGION)
 
         result = self.fetcher.get(REGION_NAME)
 
-        # reset get_for_region method
-        self.fetcher.get_for_region = original_method
+        # reset get_networks method
+        self.fetcher.get_networks = original_method
         self.assertNotEqual(result, [], "Can't get region networks info")
 
     def test_get_with_wrong_token(self):
