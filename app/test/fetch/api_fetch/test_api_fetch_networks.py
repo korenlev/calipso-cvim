dcc6066e48a319618ee8dd96018f250e4a10c13e
xiaocdon <xiaocdon@cisco.com>
2017-06-26 13:58:39 -0700

    made the following changes in test_api_fetch_networks: 1. removed redundant comments in the test 2. fixed the testing logic in test_get_networks and test_get test cases 3. fixed long line

--- app/test/fetch/api_fetch/test_api_fetch_networks.py
+++ app/test/fetch/api_fetch/test_api_fetch_networks.py
@@ -14,56 +14,43 @@ class TestApiFetchNetworks(TestFetch):
         self.set_regions_for_fetcher(self.fetcher)
 
     def test_get_networks(self):
-        # mock endpoint
         self.fetcher.get_region_url_nover = MagicMock(return_value=ENDPOINT)
-        # mock networks response and subnet response
-        self.fetcher.get_url = MagicMock(side_effect=[NETWORKS_RESPONSE, SUBNETS_RESPONSE])
-        # mock project info
+        self.fetcher.get_url = MagicMock(side_effect=[NETWORKS_RESPONSE,
+                                                      SUBNETS_RESPONSE])
         self.fetcher.inv.get_by_id = MagicMock(return_value=PROJECT)
-
         result = self.fetcher.get_networks(REGION_NAME, TOKEN)
-        self.assertNotEqual(result, [], "Can't get networks info")
-        self.assertIn("project", result[0], "Can't put project info in the network doc")
+        self.assertEqual(result, NETWORKS_RESULT, "Can't get networks info")
 
     def test_get_networks_with_wrong_networks_response(self):
-        # mock endpoint
         self.fetcher.get_region_url_nover = MagicMock(return_value=ENDPOINT)
-        # mock the wrong networks response
         self.fetcher.get_url = MagicMock(return_value=WRONG_NETWORK_RESPONSE)
 
         result = self.fetcher.get_networks(REGION_NAME, TOKEN)
-        self.assertEqual(result, [], "Can't get empty empty when the networks response is wrong")
+        self.assertEqual(result, [], "Can't get [] when the networks " +
+                                     "response is wrong")
 
     def test_get_networks_with_wrong_subnet_response(self):
-        # mock endpoint
         self.fetcher.get_region_url_nover = MagicMock(return_value=ENDPOINT)
-        # mock the correct networks response and wrong subnets response
-        self.fetcher.get_url = MagicMock(side_effect=[NETWORKS_RESPONSE, WRONG_SUBNETS_RESPONSE])
-        # mock project info
+        self.fetcher.get_url = MagicMock(side_effect=[NETWORKS_RESPONSE,
+                                                      WRONG_SUBNETS_RESPONSE])
         self.fetcher.inv.get_by_id = MagicMock(return_value=PROJECT)
 
         result = self.fetcher.get_networks(REGION_NAME, TOKEN)
 
-        self.assertNotEqual(result, [], "Can't get networks info when the subnets response is wrong")
+        self.assertNotEqual(result, [], "Can't get networks info when the " +
+                                        "subnet response is wrong")
 
     def test_get(self):
-        # store original get_networks method
         original_method = self.fetcher.get_networks
-        # mock the get_networks method
-        self.fetcher.get_networks = MagicMock(return_value=GET_FOR_REGION)
-
+        self.fetcher.get_networks = MagicMock(return_value=NETWORKS_RESULT)
         result = self.fetcher.get(REGION_NAME)
 
-        # reset get_networks method
         self.fetcher.get_networks = original_method
-        self.assertNotEqual(result, [], "Can't get region networks info")
+        self.assertEqual(result, NETWORKS_RESULT, "Can't get region networks info")
 
     def test_get_with_wrong_token(self):
-        # mock the token
         self.fetcher.v2_auth_pwd = MagicMock(return_value=None)
-
         result = self.fetcher.get(REGION_NAME)
-
-        # reset token
         self.fetcher.v2_auth_pwd = MagicMock(return_value=TOKEN)
-        self.assertEqual(result, [], "Can't get empty array when the token is invalid")
+        self.assertEqual(result, [], "Can't get [] when the " +
+                                     "token is invalid")
