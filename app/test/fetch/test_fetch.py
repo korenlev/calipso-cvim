8eddd14c79726b73d810774b589b69a49323fa52
xiaocdon <xiaocdon@cisco.com>
2017-06-27 13:54:42 -0700

    used the mongo config path in the test_config file to initialize the MongoAccess

--- app/test/fetch/test_fetch.py
+++ app/test/fetch/test_fetch.py
@@ -2,11 +2,12 @@ import unittest
 
 from discover.configuration import Configuration
 from discover.db_access import DbAccess
-from test.fetch.config.test_config import ENV_CONFIG, COLLECTION_CONFIG
+from test.fetch.config.test_config import MONGODB_CONFIG, ENV_CONFIG, COLLECTION_CONFIG
 from test.fetch.api_fetch.test_data.regions import REGIONS
 from test.fetch.api_fetch.test_data.configurations import CONFIGURATIONS
 from unittest.mock import MagicMock
 from utils.inventory_mgr import InventoryMgr
+from utils.mongo_access import MongoAccess
 
 
 class TestFetch(unittest.TestCase):
@@ -14,7 +15,7 @@ class TestFetch(unittest.TestCase):
     def configure_environment(self):
         self.env = ENV_CONFIG
         self.inventory_collection = COLLECTION_CONFIG
-
+        MongoAccess.config_file = MONGODB_CONFIG
         self.conf = Configuration()
         self.conf.use_env = MagicMock()
         self.conf.environment = CONFIGURATIONS
