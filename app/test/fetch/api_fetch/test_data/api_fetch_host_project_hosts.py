b445c9f7e2b83c6882725bd392c6ddf1e9ca838e
xiaocdon <xiaocdon@cisco.com>
2017-06-27 23:02:51 -0700

    removed unused data and fields from the test_data of api_fetch_host_project

--- app/test/fetch/api_fetch/test_data/api_fetch_host_project_hosts.py
+++ app/test/fetch/api_fetch/test_data/api_fetch_host_project_hosts.py
@@ -9,22 +9,18 @@ HOST_DOC = {
         "nova-cert": {
             "active": True,
             "available": True,
-            "updated_at": "2016-10-21T18:12:56.000000"
         },
         "nova-conductor": {
             "active": True,
             "available": True,
-            "updated_at": "2016-10-21T18:12:46.000000"
         },
         "nova-consoleauth": {
             "active": True,
             "available": True,
-            "updated_at": "2016-10-21T18:12:54.000000"
         },
         "nova-scheduler": {
             "active": True,
             "available": True,
-            "updated_at": "2016-10-21T18:13:10.000000"
         }
     },
     "zone": "internal"
@@ -35,26 +31,10 @@ COMPUTE_TYPE = "Compute"
 ZONE = "internal"
 HOST_ZONE = "Test"
 
-TOKEN = {'tenant': {
-    'description': 'admin tenant',
-    'name': 'admin',
-    'is_domain': False,
-    'id': '8c1751e0ce714736a63fee3c776164da',
-    'enabled': True,
-    'parent_id': None
-    },
-    'issued_at': '2016-10-19T23:06:29.000000Z',
-    'expires': '2016-10-20T00:06:28.615780Z',
-    'id': 'gAAAAABYB_x10x_6AlA2Y5RJZ6HCcCDSXe0f8vfisKnOM_XCDZvwl2qiwzCQIOYX9mCmRyGojZ2JEjIb0vHL0f0hxqSq84g5jbZpN0h0Un_RkTZXSKf0K1uigbr3q__ilhctLvwWNem6XQSGrav1fQrec_DjdvUxSwuoBSSo82kKQ7SvPSdVwrA',
-    'token_expiry_time': 1476921988,
-    'audit_ids': ['2Ps0lRlHRIG80FWamMkwWg']
-}
-
 REGION_NAME = "RegionOne"
 TEST_PROJECT_NAME = "Test"
 PROJECT_NAME = "admin"
 
-ENDPOINT = "http://10.56.20.239:8774/v2/8c1751e0ce714736a63fee3c776164da"
 AVAILABILITY_ZONE_RESPONSE = {
     "availabilityZoneInfo": [
         {
@@ -63,22 +43,18 @@ AVAILABILITY_ZONE_RESPONSE = {
                     "nova-cert": {
                         "active": True,
                         "available": True,
-                        "updated_at": "2016-10-21T18:22:56.000000"
                     },
                     "nova-conductor": {
                         "active": True,
                         "available": True,
-                        "updated_at": "2016-10-21T18:22:46.000000"
                     },
                     "nova-consoleauth": {
                         "active": True,
                         "available": True,
-                        "updated_at": "2016-10-21T18:22:54.000000"
                     },
                     "nova-scheduler": {
                         "active": True,
                         "available": True,
-                        "updated_at": "2016-10-21T18:22:10.000000"
                     }
                 }
             },
@@ -93,26 +69,10 @@ AVAILABILITY_ZONE_RESPONSE = {
                     "nova-compute": {
                         "active": True,
                         "available": True,
-                        "updated_at": "2016-10-21T18:22:42.000000"
                     }
                 }
             },
             "zoneName": "osdna-zone",
-            "zoneState": {
-                "available": True
-            }
-        },
-        {
-            "hosts": {
-                "node-4.cisco.com": {
-                    "nova-compute": {
-                        "active": True,
-                        "available": True,
-                        "updated_at": "2016-10-21T18:22:44.000000"
-                    }
-                }
-            },
-            "zoneName": "nova",
             "zoneState": {
                 "available": True
             }
@@ -123,42 +83,14 @@ AVAILABILITY_ZONE_RESPONSE = {
 AVAILABILITY_ERROR_RESPONSE = {'status': 400}
 
 HYPERVISORS_RESPONSE = {
-    "hypervisors": [
-        {
-            "hypervisor_hostname": "node-5.cisco.com",
-            "id": 1,
-            "state": "up",
-            "status": "enabled"
-        },
-        {
-            "hypervisor_hostname": "node-4.cisco.com",
-            "id": 2,
-            "state": "up",
-            "status": "enabled"
-        }
-    ]
+    "hypervisors": []
 }
 
 HYPERVISORS_ERROR_RESPONSE = {'status': 400}
 
 HOST_TO_BE_FETCHED_IP = {
     "host": "node-5.cisco.com",
-    "host_type": [
-        "Compute"
-    ],
-    "id": "node-5.cisco.com",
-    "name": "node-5.cisco.com",
-    "os_id": "1",
-    "parent_id": "osdna-zone",
-    "parent_type": "availability_zone",
-    "services": {
-        "nova-compute": {
-            "active": True,
-            "available": True,
-            "updated_at": "2016-10-21T18:22:42.000000"
-        }
-    },
-    "zone": "osdna-zone"
+    "id": "node-5.cisco.com"
 }
 
 IP_ADDRESS_RESPONSE = [
@@ -175,31 +107,6 @@ HOSTS_TO_BE_FETCHED_NETWORK_DETAILS = [
         ],
         "id": "node-6.cisco.com",
         "name": "node-6.cisco.com",
-        "parent_id": "internal",
-        "parent_type": "availability_zone",
-        "services": {
-            "nova-cert": {
-                "active": True,
-                "available": True,
-                "updated_at": "2016-10-21T18:22:56.000000"
-            },
-            "nova-conductor": {
-                "active": True,
-                "available": True,
-                "updated_at": "2016-10-21T18:22:46.000000"
-            },
-            "nova-consoleauth": {
-                "active": True,
-                "available": True,
-                "updated_at": "2016-10-21T18:22:54.000000"
-            },
-            "nova-scheduler": {
-                "active": True,
-                "available": True,
-                "updated_at": "2016-10-21T18:22:10.000000"
-            }
-        },
-        "zone": "internal"
     },
     {
         "host": "node-5.cisco.com",
@@ -207,59 +114,22 @@ HOSTS_TO_BE_FETCHED_NETWORK_DETAILS = [
             "Compute"
         ],
         "id": "node-5.cisco.com",
-        "ip_address": "192.168.0.4",
         "name": "node-5.cisco.com",
-        "os_id": "1",
-        "parent_id": "osdna-zone",
-        "parent_type": "availability_zone",
-        "services": {
-            "nova-compute": {
-                "active": True,
-                "available": True,
-                "updated_at": "2016-10-21T18:22:42.000000"
-            }
-        },
-        "zone": "osdna-zone"
-    },
-    {
-        "host": "node-4.cisco.com",
-        "host_type": [
-            "Compute"
-        ],
-        "id": "node-4.cisco.com",
-        "ip_address": "192.168.0.5",
-        "name": "node-4.cisco.com",
-        "os_id": "2",
-        "parent_id": "nova",
-        "parent_type": "availability_zone",
-        "services": {
-            "nova-compute": {
-                "active": True,
-                "available": True,
-                "updated_at": "2016-10-21T18:22:44.000000"
-            }
-        },
-        "zone": "nova"
     }
 ]
 
-
-
 NETWORKS_DETAILS_RESPONSE = [
     {
-        "configurations": "{\"subnets\": 9, \"use_namespaces\": true, \"dhcp_lease_duration\": 600, \"dhcp_driver\": \"neutron.agent.linux.dhcp.Dnsmasq\", \"ports\": 19, \"log_agent_heartbeats\": false, \"networks\": 9}",
-        "host": "node-6.cisco.com",
-        "id": "node-6.cisco.com"
+        "configurations": "{}",
+        "host": "node-6.cisco.com"
     },
     {
-        "configurations": "{\"log_agent_heartbeats\": false, \"nova_metadata_ip\": \"192.168.0.2\", \"nova_metadata_port\": 8775, \"metadata_proxy_socket\": \"/var/lib/neutron/metadata_proxy\"}",
+        "configurations": "{}",
         "host": "node-6.cisco.com",
-        "id": "node-6.cisco.com"
     },
     {
-        "configurations": "{\"router_id\": \"\", \"agent_mode\": \"legacy\", \"gateway_external_network_id\": \"\", \"handle_internal_only_routers\": true, \"use_namespaces\": true, \"routers\": 2, \"interfaces\": 4, \"floating_ips\": 1, \"interface_driver\": \"neutron.agent.linux.interface.OVSInterfaceDriver\", \"log_agent_heartbeats\": false, \"external_network_bridge\": \"\", \"ex_gw_ports\": 2}",
+        "configurations": "{}",
         "host": "node-6.cisco.com",
-        "id": "node-6.cisco.com"
     }
 ]
 
@@ -270,48 +140,29 @@ AVAILABILITY_ZONE = {
                     "nova-cert": {
                         "active": True,
                         "available": True,
-                        "updated_at": "2016-10-21T18:22:56.000000"
                     },
                     "nova-conductor": {
                         "active": True,
                         "available": True,
-                        "updated_at": "2016-10-21T18:22:46.000000"
                     },
                     "nova-consoleauth": {
                         "active": True,
                         "available": True,
-                        "updated_at": "2016-10-21T18:22:54.000000"
                     },
                     "nova-scheduler": {
                         "active": True,
                         "available": True,
-                        "updated_at": "2016-10-21T18:22:10.000000"
                     }
                 }
             },
-            "zoneName": "internal",
-            "zoneState": {
-                "available": True
-            }
-        }
+    "zoneName": "internal"
+}
 
 HOST_NAME = "node-6.cisco.com"
 
 GET_FOR_REGION_INFO = [
     {
         "config": {
-            "agent_mode": "legacy",
-            "ex_gw_ports": 2,
-            "external_network_bridge": "",
-            "floating_ips": 1,
-            "gateway_external_network_id": "",
-            "handle_internal_only_routers": True,
-            "interface_driver": "neutron.agent.linux.interface.OVSInterfaceDriver",
-            "interfaces": 4,
-            "log_agent_heartbeats": False,
-            "router_id": "",
-            "routers": 2,
-            "use_namespaces": True
         },
         "host": "node-6.cisco.com",
         "host_type": [
@@ -326,22 +177,18 @@ GET_FOR_REGION_INFO = [
             "nova-cert": {
                 "active": True,
                 "available": True,
-                "updated_at": "2016-10-21T18:22:56.000000"
             },
             "nova-conductor": {
                 "active": True,
                 "available": True,
-                "updated_at": "2016-10-21T18:22:46.000000"
             },
             "nova-consoleauth": {
                 "active": True,
                 "available": True,
-                "updated_at": "2016-10-21T18:22:54.000000"
             },
             "nova-scheduler": {
                 "active": True,
                 "available": True,
-                "updated_at": "2016-10-21T18:22:10.000000"
             }
         },
         "zone": "internal"
@@ -365,120 +212,5 @@ GET_FOR_REGION_INFO = [
             }
         },
         "zone": "osdna-zone"
-    },
-    {
-        "host": "node-4.cisco.com",
-        "host_type": [
-            "Compute"
-        ],
-        "id": "node-4.cisco.com",
-        "ip_address": "192.168.0.5",
-        "name": "node-4.cisco.com",
-        "os_id": "2",
-        "parent_id": "nova",
-        "parent_type": "availability_zone",
-        "services": {
-            "nova-compute": {
-                "active": True,
-                "available": True,
-                "updated_at": "2016-10-21T18:22:44.000000"
-            }
-        },
-        "zone": "nova"
     }
 ]
-
-# FUNCTIONAL TEST
-INPUT = "admin"
-
-RESULT = [
-    {
-        "config": {
-            "agent_mode": "legacy",
-            "ex_gw_ports": 2,
-            "external_network_bridge": "",
-            "floating_ips": 1,
-            "gateway_external_network_id": "",
-            "handle_internal_only_routers": True,
-            "interface_driver": "neutron.agent.linux.interface.OVSInterfaceDriver",
-            "interfaces": 4,
-            "log_agent_heartbeats": False,
-            "router_id": "",
-            "routers": 2,
-            "use_namespaces": True
-        },
-        "host": "node-6.cisco.com",
-        "host_type": [
-            "Controller",
-            "Network"
-        ],
-        "id": "node-6.cisco.com",
-        "name": "node-6.cisco.com",
-        "parent_id": "internal",
-        "parent_type": "availability_zone",
-        "services": {
-            "nova-cert": {
-                "active": True,
-                "available": True,
-                "updated_at": "2016-10-21T18:12:56.000000"
-            },
-            "nova-conductor": {
-                "active": True,
-                "available": True,
-                "updated_at": "2016-10-21T18:12:46.000000"
-            },
-            "nova-consoleauth": {
-                "active": True,
-                "available": True,
-                "updated_at": "2016-10-21T18:12:54.000000"
-            },
-            "nova-scheduler": {
-                "active": True,
-                "available": True,
-                "updated_at": "2016-10-21T18:13:10.000000"
-            }
-        },
-        "zone": "internal"
-    },
-    {
-        "host": "node-5.cisco.com",
-        "host_type": [
-            "Compute"
-        ],
-        "id": "node-5.cisco.com",
-        "ip_address": "192.168.0.4",
-        "name": "node-5.cisco.com",
-        "os_id": "1",
-        "parent_id": "osdna-zone",
-        "parent_type": "availability_zone",
-        "services": {
-            "nova-compute": {
-                "active": True,
-                "available": True,
-                "updated_at": "2016-10-21T18:12:42.000000"
-            }
-        },
-        "zone": "osdna-zone"
-    },
-    {
-        "host": "node-4.cisco.com",
-        "host_type": [
-            "Compute"
-        ],
-        "id": "node-4.cisco.com",
-        "ip_address": "192.168.0.5",
-        "name": "node-4.cisco.com",
-        "os_id": "2",
-        "parent_id": "nova",
-        "parent_type": "availability_zone",
-        "services": {
-            "nova-compute": {
-                "active": True,
-                "available": True,
-                "updated_at": "2016-10-21T18:12:44.000000"
-            }
-        },
-        "zone": "nova"
-    }
-]
-
