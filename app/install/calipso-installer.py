b64f433a1a5aead29c6341d6aca5adaf46d6a02e
koren <korlev@cisco.com>
2017-07-04 18:07:07 +0300

    added note regarding docker security lib tbd later

--- app/install/calipso-installer.py
+++ app/install/calipso-installer.py
@@ -1,17 +1,18 @@
--########################################################################################
--# Copyright (c) 2017 Koren Lev (Cisco Systems), Yaron Yogev (Cisco Systems) and others #
--#                                                                                      #
--# All rights reserved. This program and the accompanying materials                     #
--# are made available under the terms of the Apache License, Version 2.0                #
--# which accompanies this distribution, and is available at                             #
--# http://www.apache.org/licenses/LICENSE-2.0                                           #
--########################################################################################
+########################################################################################
+# Copyright (c) 2017 Koren Lev (Cisco Systems), Yaron Yogev (Cisco Systems) and others #
+#                                                                                      #
+# All rights reserved. This program and the accompanying materials                     #
+# are made available under the terms of the Apache License, Version 2.0                #
+# which accompanies this distribution, and is available at                             #
+# http://www.apache.org/licenses/LICENSE-2.0                                           #
+########################################################################################
 from pymongo import MongoClient, ReturnDocument
 from pymongo.errors import ConnectionFailure
 from urllib.parse import quote_plus
 import docker
 import argparse
 import dockerpycreds
+# note : not used, useful for docker api security if used
 import time
 import json
 
