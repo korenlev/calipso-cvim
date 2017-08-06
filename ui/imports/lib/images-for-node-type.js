75170fac87b52b9f65b17955fa25b00a53eac898
Koren Lev <korlev@cisco.com>
2017-08-06 11:39:20 +0300

    adding latest ui code, moved from 'Calipso_UI repo

---
+++ ui/imports/lib/images-for-node-type.js
@@ -0,0 +1,14 @@
+export let imagesForNodeType = {
+  'instance': 'ic_computer_black_48dp_2x.png',
+  'pnic': 'ic_dns_black_48dp_2x.png',
+  'vconnector': 'ic_settings_input_composite_black_48dp_2x.png',
+  // 'network': 'ic_cloud_queue_black_48dp_2x.png',
+  'network': 'ic_cloud_queue_black_48dp_2x.png',
+  'vedge': 'ic_gamepad_black_48dp_2x.png',
+  'vservice': 'ic_storage_black_48dp_2x.png',
+  'vnic': 'ic_settings_input_hdmi_black_48dp_2x.png',
+  'otep':'ic_keyboard_return_black_48dp_2x.png',
+};
+
+export let defaultNodeTypeImage = 'ic_lens_black_48dp_2x.png';
+
