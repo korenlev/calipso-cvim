5d5e213fb3b29e4986a252eaea97988d9f058279
korlev <korlev@cisco.com>
2017-09-27 19:15:05 +0300

    mercury specific cli handling for dhcp and router vservices

--- app/utils/cli_dist_translator.py
+++ app/utils/cli_dist_translator.py
@@ -4,7 +4,10 @@ class CliDistTranslator:
 
     TRANSLATIONS = {
         'Mercury': {
-           'ip netns': '{docker_call} neutron_l3_agent_{version} {cmd}',
+            'ip netns list': '{docker_call} neutron_l3_agent_{version} {cmd} && '
+                             '{docker_call} neutron_dhcp_agent_{version} {cmd}',
+            'ip netns exec qdhcp': '{docker_call} neutron_dhcp_agent_{version} {cmd}',
+            'ip netns exec qrouter': '{docker_call} neutron_l3_agent_{version} {cmd}',
             'virsh': '{docker_call} novalibvirt_{version} {cmd}',
             'ip link': '{docker_call} ovs_vswitch_{version} {cmd}',
             'ip -d link': '{docker_call} ovs_vswitch_{version} {cmd}',
