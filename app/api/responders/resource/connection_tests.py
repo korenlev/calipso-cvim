a2f3f1986c12fc16593642a0c089ede9e5bc9a7c
Ilia Abashin <iabashin@cisco.com>
2017-10-02 19:22:14 +0300

    connection_tests list query implemented

--- app/api/responders/resource/connection_tests.py
+++ app/api/responders/resource/connection_tests.py
@@ -24,21 +24,29 @@ class ConnectionTests(ResponderBase):
     RESULTS = "test_results"
     CONFIGURATIONS = "targets_configuration"
     STATUSES = ["request", "response"]
-    PROJECTION = {}
 
     def __init__(self):
         super().__init__()
+        self.PROJECTION = {
+            self.ID: True,
+            self.TARGETS: True,
+            self.RESULTS: True
+        }
         self.allowed_targets = \
             self.get_constants_by_name("configuration_targets")
 
     def build_query(self, filters):
-        query = {
-            self.ID: filters['id'],
-        }
-        if 'env_name' in filters:
-            query['environment'] = filters['env_name']
+        query = {}
 
         self.update_query_with_filters(filters, ["status"], query)
+
+        if 'id' in filters:
+            query[self.ID] = filters['id']
+        elif 'env_name' in filters:
+            query['environment'] = filters['env_name']
+        else:
+            self.bad_request(message="Either 'id' or 'env_name' "
+                                     "field is required")
 
         return query
 
@@ -47,63 +55,74 @@ class ConnectionTests(ResponderBase):
         filters = self.parse_query_params(req)
 
         filters_requirements = {
-            'id': self.require(ObjectId, mandatory=True, convert_to_type=True),
-            'env_name': self.require(str),
+            'env_name': self.require(str, mandatory=True),
+            'id': self.require(ObjectId, convert_to_type=True),
             'status': self.require(str,
                                    requirement=self.STATUSES),
             self.TARGETS: self.require([list, str],
                                        validate=DataValidate.LIST,
                                        requirement=self.allowed_targets),
             self.RESULTS: self.require(bool, convert_to_type=True),
+            'page': self.require(int, convert_to_type=True),
+            'page_size': self.require(int, convert_to_type=True)
         }
 
         self.validate_query_data(filters, filters_requirements)
 
         query = self.build_query(filters)
 
-        result = self.get_object_by_id(collection=self.COLLECTION,
-                                       query=query,
-                                       stringify_types=[ObjectId,
-                                                        datetime.datetime],
-                                       id=self.ID)
+        if self.ID in query:
+            result = self.get_object_by_id(collection=self.COLLECTION,
+                                           query=query,
+                                           stringify_types=[ObjectId,
+                                                            datetime.datetime],
+                                           id=self.ID)
 
-        test_targets = result.get(self.TARGETS, [])
-        targets_config = result.get(self.CONFIGURATIONS, [])
-        test_results = result.get(self.RESULTS, {})
+            test_targets = result.get(self.TARGETS, [])
+            targets_config = result.get(self.CONFIGURATIONS, [])
+            test_results = result.get(self.RESULTS, {})
 
-        # Filter data by target names
-        targets_filter = filters.get(self.TARGETS)
-        if targets_filter:
-            test_targets = [target
-                            for target in test_targets
-                            if target in targets_filter]
-            targets_config = [config
-                              for config in targets_config
-                              if config['name'] in targets_filter]
-            test_results = {target: result
-                            for target, result in test_results.items()
-                            if target in targets_filter}
+            # Filter data by target names
+            targets_filter = filters.get(self.TARGETS)
+            if targets_filter:
+                test_targets = [target
+                                for target in test_targets
+                                if target in targets_filter]
+                targets_config = [config
+                                  for config in targets_config
+                                  if config['name'] in targets_filter]
+                test_results = {target: result
+                                for target, result in test_results.items()
+                                if target in targets_filter}
 
-        # Filter data by test results (success/failure)
-        results_filter = filters.get(self.RESULTS)
-        if results_filter is not None:
-            test_results = {target: result
-                            for target, result in test_results.items()
-                            if result == results_filter}
+            # Filter data by test results (success/failure)
+            results_filter = filters.get(self.RESULTS)
+            if results_filter is not None:
+                test_results = {target: result
+                                for target, result in test_results.items()
+                                if result == results_filter}
 
-            results_keys = test_results.keys()
-            test_targets = [target
-                            for target in test_targets
-                            if target in results_keys]
-            targets_config = [config
-                              for config in targets_config
-                              if config['name'] in results_keys]
+                results_keys = test_results.keys()
+                test_targets = [target
+                                for target in test_targets
+                                if target in results_keys]
+                targets_config = [config
+                                  for config in targets_config
+                                  if config['name'] in results_keys]
 
-        result[self.TARGETS] = test_targets
-        result[self.CONFIGURATIONS] = targets_config
-        result[self.RESULTS] = test_results
+            result[self.TARGETS] = test_targets
+            result[self.CONFIGURATIONS] = targets_config
+            result[self.RESULTS] = test_results
 
-        self.set_successful_response(resp, result)
+            self.set_successful_response(resp, result)
+        else:
+            page, page_size = self.get_pagination(filters)
+            tests_ids = self.get_objects_list(collection=self.COLLECTION,
+                                              query=query,
+                                              page=page,
+                                              page_size=page_size,
+                                              projection=self.PROJECTION)
+            self.set_successful_response(resp, {"connection_tests": tests_ids})
 
     def on_post(self, req, resp):
         self.log.debug("Posting a new connection test")
