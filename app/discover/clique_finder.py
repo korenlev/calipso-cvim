586e03f986f94971ba837ecefc76b561ac8b3e4a
Yaron Yogev <yayogev@cisco.com>
2017-10-29 16:50:01 +0200

    US3128 use implicit links only if this is specified in clique type
    Clique type must have attribute "use_implicit_links" set to True
    for implicit links to be used in cliques

--- app/discover/clique_finder.py
+++ app/discover/clique_finder.py
@@ -141,8 +141,10 @@ class CliqueFinder(Fetcher):
         for c in constraints:
             val = o[c] if c in o else None
             clique["constraints"][c] = val
+        allow_implicit = clique_type.get('use_implicit_links', False)
         for link_type in clique_type["link_types"]:
-            self.check_link_type(clique, link_type, nodes_of_type)
+            self.check_link_type(clique, link_type, nodes_of_type,
+                                 allow_implicit=allow_implicit)
 
         # after adding the links to the clique, create/update the clique
         if not clique["links"]:
@@ -193,25 +195,32 @@ class CliqueFinder(Fetcher):
                 '-'.join(link_type_parts)
         return CliqueFinder.link_type_reversed.get(link_type)
 
-    def check_link_type(self, clique, link_type, nodes_of_type):
+    def check_link_type(self, clique, link_type, nodes_of_type,
+                        allow_implicit=False):
         # check if it's backwards
         link_type_reversed = self.get_link_type_reversed(link_type)
         # handle case of links like T<-->T
         self_linked = link_type == link_type_reversed
         use_reversed = False
         if not self_linked:
-            matches = self.links.find_one({
+            link_search_condition = {
                 "environment": self.env,
                 "link_type": link_type_reversed
-            })
+            }
+            if not allow_implicit:
+                link_search_condition['implicit'] = False
+            matches = self.links.find_one(link_search_condition)
             use_reversed = True if matches else False
         if self_linked or not use_reversed:
-            self.check_link_type_forward(clique, link_type, nodes_of_type)
+            self.check_link_type_forward(clique, link_type, nodes_of_type,
+                                         allow_implicit=allow_implicit)
         if self_linked or use_reversed:
-            self.check_link_type_back(clique, link_type, nodes_of_type)
+            self.check_link_type_back(clique, link_type, nodes_of_type,
+                                      allow_implicit=allow_implicit)
 
     def check_link_type_for_direction(self, clique, link_type, nodes_of_type,
-                                      is_reversed=False):
+                                      is_reversed=False,
+                                      allow_implicit=False):
         if is_reversed:
             link_type = self.get_link_type_reversed(link_type)
         from_type = link_type[:link_type.index("-")]
@@ -228,7 +237,8 @@ class CliqueFinder(Fetcher):
                                                   clique,
                                                   link_type,
                                                   side_to_match,
-                                                  other_side)
+                                                  other_side,
+                                                  allow_implicit=allow_implicit)
             nodes_to_add = nodes_to_add | matches
         if other_side_type not in nodes_of_type:
             nodes_of_type[other_side_type] = set()
@@ -236,13 +246,17 @@ class CliqueFinder(Fetcher):
             nodes_of_type[other_side_type] | nodes_to_add
 
     def find_matches_for_point(self, match_point, clique, link_type,
-                               side_to_match, other_side) -> set:
+                               side_to_match, other_side,
+                               allow_implicit=False) -> set:
         nodes_to_add = set()
-        matches = self.links.find({
+        link_search_condition = {
             "environment": self.env,
             "link_type": link_type,
             side_to_match: ObjectId(match_point)
-        })
+        }
+        if not allow_implicit:
+            link_search_condition['implicit'] = False
+        matches = self.links.find(link_search_condition)
         for link in matches:
             link_id = link["_id"]
             if link_id in clique["links"]:
@@ -255,10 +269,14 @@ class CliqueFinder(Fetcher):
             nodes_to_add.add(other_side_point)
         return nodes_to_add
 
-    def check_link_type_forward(self, clique, link_type, nodes_of_type):
+    def check_link_type_forward(self, clique, link_type, nodes_of_type,
+                                allow_implicit=False):
         self.check_link_type_for_direction(clique, link_type, nodes_of_type,
-                                           is_reversed=False)
+                                           is_reversed=False,
+                                           allow_implicit=allow_implicit)
 
-    def check_link_type_back(self, clique, link_type, nodes_of_type):
+    def check_link_type_back(self, clique, link_type, nodes_of_type,
+                             allow_implicit=False):
         self.check_link_type_for_direction(clique, link_type, nodes_of_type,
-                                           is_reversed=True)
+                                           is_reversed=True,
+                                           allow_implicit=allow_implicit)
