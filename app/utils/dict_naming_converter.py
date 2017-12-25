30f59c519c9b42792b2bf82b713629f0da631798
Yaron Yogev <yayogev@cisco.com>
2017-12-25 10:22:31 +0200

    change_dict_naming_convention(): handle object with attributes

--- app/utils/dict_naming_converter.py
+++ app/utils/dict_naming_converter.py
@@ -22,19 +22,27 @@ class DictNamingConverter:
     @staticmethod
     def change_dict_naming_convention(d, cf):
         new = {}
+        change_convention = DictNamingConverter.change_dict_naming_convention
         if not d:
             return d
-        if isinstance(d, str):
+        if isinstance(d, str) or isinstance(d, int) or isinstance(d, float) \
+                or isinstance(d, bool):
             return d
         if isinstance(d, ObjectId):
             return d
-        for k, v in d.items():
-            new_v = v
-            if isinstance(v, dict):
-                new_v = DictNamingConverter.change_dict_naming_convention(v, cf)
-            elif isinstance(v, list):
-                new_v = list()
-                for x in v:
-                    new_v.append(DictNamingConverter.change_dict_naming_convention(x, cf))
-            new[cf(k)] = new_v
+        if isinstance(d, object) and not isinstance(d, dict):
+            for k in [k for k in dir(d) if not k.startswith('_')]:
+                v = getattr(d, k)
+                new[cf(k)] = change_convention(v, cf)
+        if isinstance(d, dict):
+            for k, v in d.items():
+                new_v = v
+                if isinstance(v, dict):
+                    new_v = change_convention(v, cf)
+                elif isinstance(v, list):
+                    new_v = list()
+                    for x in v:
+                        list_val = change_convention(x, cf)
+                        new_v.append(list_val)
+                new[cf(k)] = new_v
         return new
