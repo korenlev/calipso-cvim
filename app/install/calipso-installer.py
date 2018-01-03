2457363f9a98f7fb6e9a0221ccc1800348ab7b2c
Koren Lev <korenlev@gmail.com>
2018-01-03 09:44:15 +0200

    bug fix, ifconfig for docker 0 in latest kernels

--- app/install/calipso-installer.py
+++ app/install/calipso-installer.py
@@ -20,7 +20,7 @@ import socket
 # by default, we want to use the docker0 interface ip address for inter-contatiner communications,
 # if hostname argument will not be provided as argument for the calipso-installer
 import os
-dockerip = os.popen('ifconfig docker0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
+dockerip = os.popen('ifconfig docker0 | grep "inet" | cut -d: -f10 | cut -d" " -f10')
 local_hostname = dockerip.read().replace("\n", "")
 
 C_MONGO_CONFIG = "/local_dir/calipso_mongo_access.conf"
