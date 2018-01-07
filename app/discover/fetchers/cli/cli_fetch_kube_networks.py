b8e231a4e34d2c6d02044906a363f11bb9b5df54
Yaron Yogev <yayogev@cisco.com>
2018-01-07 16:57:40 +0200

    US3451 Add Kubernetes network scanning

---
+++ app/discover/fetchers/cli/cli_fetch_kube_networks.py
@@ -0,0 +1,65 @@
+###############################################################################
+# Copyright (c) 2017 Koren Lev (Cisco Systems), Yaron Yogev (Cisco Systems)   #
+# and others                                                                  #
+#                                                                             #
+# All rights reserved. This program and the accompanying materials            #
+# are made available under the terms of the Apache License, Version 2.0       #
+# which accompanies this distribution, and is available at                    #
+# http://www.apache.org/licenses/LICENSE-2.0                                  #
+###############################################################################
+import json
+from json import JSONDecodeError
+
+from discover.fetchers.cli.cli_access import CliAccess
+from utils.inventory_mgr import InventoryMgr
+
+
+class CliFetchKubeNetworks(CliAccess):
+    def __init__(self):
+        super().__init__()
+        self.inv = InventoryMgr()
+
+    def get(self, host_id: str) -> list:
+        host = self.inv.get_by_id(self.get_env(), host_id)
+        if not host:
+            self.log.error('Failed to find host with ID: {}', host_id)
+            return []
+        lines = self.run_fetch_lines('docker network ls', host_id)
+        ret = []
+        HEADERS = [
+            'NETWORK ID',
+            'NAME',
+            'DRIVER',
+            'SCOPE'
+        ]
+        networks = self.parse_cmd_result_with_whitespace(lines, HEADERS, True)
+        for network in networks:
+            ret.append(self.get_network(host, network))
+        return ret
+
+    def get_network(self, host, network_data) -> dict:
+        network = {
+            'host': host['host'],
+            'id': network_data['NETWORK ID'],
+            'name': network_data['NAME'],
+            'driver': network_data['DRIVER'],
+            'scope': network_data['SCOPE']
+        }
+        self.set_folder_parent(network, 'network',
+                               master_parent_type='host',
+                               master_parent_id=host['id'])
+        self.get_network_data(network)
+        return network
+
+    def get_network_data(self, network):
+        cmd = 'docker network inspect {}'.format(network['id'])
+        output = self.run(cmd, network['host'])
+        try:
+            network_data = json.loads(output)
+        except JSONDecodeError as e:
+            self.log.error('error reading network data for {}: {}'
+                           .format(network['id'], str(e)))
+        network_data = network_data[0]
+        network_data.pop('Id')
+        network_data.pop('Name')
+        network.update(network_data)
