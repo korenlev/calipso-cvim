afbaa2f5eec94c0b3a4c243d340e6a23ec1107c3
Ilia Abashin <iabashin@cisco.com>
2018-02-27 16:20:13 +0300

    Configuration options by environment type (UI)

--- ui/imports/ui/lib/select-model.js
+++ ui/imports/ui/lib/select-model.js
@@ -2,6 +2,7 @@ import * as R from 'ramda';
 
 export const createSelectArgs = function (params) {
   let instance = Template.instance();
+  let reset_fields = params.hash.reset_fields ? params.hash.reset_fields : [];
 
   return {
     values: params.hash.values,
@@ -10,10 +11,16 @@ export const createSelectArgs = function (params) {
     options: params.hash.options,
     multi: params.hash.multi ? params.hash.multi : false, 
     disabled: params.hash.disabled,
+    reset_fields: reset_fields,
     setModel: params.hash.setModel ? params.hash.setModel.fn : 
       function (values) {
         let model = instance.data.model; 
         let newModel = R.assoc(params.hash.key, values, model);
+        // Workaround for resetting independent model fields
+        reset_fields.forEach(function(field) {
+            // TODO: provide default value? Empty array may be fine though
+            newModel = R.assoc(field, [], newModel);
+        });
         if (instance.data.setModel) {
           instance.data.setModel(newModel); 
         }
