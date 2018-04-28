c09edbaab99992b7e15332b755b05dfe88fd3177
Yaron Yogev <yayogev@cisco.com>
2018-04-28 17:44:11 +0300

    US3826 moved fetching of interface hardware details to ext.class

--- app/discover/fetchers/kube/kube_fetch_vnics_vpp.py
+++ app/discover/fetchers/kube/kube_fetch_vnics_vpp.py
@@ -8,6 +8,8 @@
 # http://www.apache.org/licenses/LICENSE-2.0                                  #
 ###############################################################################
 from discover.fetchers.cli.cli_fetcher import CliFetcher
+from discover.fetchers.cli.cli_fetch_interface_hardware_details_vpp \
+    import CliFetchInterfaceHardwareDetailsVpp
 from utils.inventory_mgr import InventoryMgr
 
 
@@ -15,6 +17,11 @@ class KubeFetchVnicsVpp(CliFetcher):
     def __init__(self):
         super().__init__()
         self.inv = InventoryMgr()
+        self.if_details_fetcher = CliFetchInterfaceHardwareDetailsVpp()
+
+    def set_env(self, env):
+        super().set_env(env)
+        self.if_details_fetcher.set_env(env)
 
     def get(self, host_id: str) -> list:
         cmd = 'vppctl show ip arp'
@@ -29,7 +36,7 @@ class KubeFetchVnicsVpp(CliFetcher):
             interface = self.process_interface(host_id, line)
             if interface:
                 ret.append(self.process_interface(host_id, line))
-        self.add_hardware_interfaces_details(host_id, ret)
+        self.if_details_fetcher.add_hardware_interfaces_details(host_id, ret)
         return ret
 
     def process_interface(self, host_id, interface_details) -> dict:
@@ -49,33 +56,3 @@ class KubeFetchVnicsVpp(CliFetcher):
             return dict()
         interface['id'] = '{}-{}'.format(host_id, interface['name'])
         return interface
-
-    def add_hardware_interfaces_details(self, host_id: str, interfaces: list):
-        if not interfaces:
-            return
-        cmd = "vppctl show hardware-interfaces"
-        lines = self.run_fetch_lines(cmd, ssh_to_host=host_id)
-        for interface in interfaces:
-            self.add_hardware_interface_details(interface, lines)
-
-    def add_hardware_interface_details(self, interface: dict, lines: list):
-        interface_lines = []
-        # find the lines for this interface
-        for l in lines:
-            details = l.split()
-            if not details:
-                continue
-            if details[0] == interface['name']:
-                interface_lines = [lines[0], l]   # headers line & first line
-                interface['index'] = details[1]
-            elif not interface_lines:
-                continue
-            elif l.startswith(' '):
-                interface_lines.append(l)
-                if l.strip().startswith('Ethernet address'):
-                    interface['mac_address'] = l.split()[-1]
-            else:
-                break
-        if not interface_lines:
-            return
-        interface['hardware_details'] = '\n'.join(interface_lines)
