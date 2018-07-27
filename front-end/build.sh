a307128866818f4119180fd3f8e2c5b3eb6ba003
Koren Lev <korlev@cisco.com>
2018-07-27 10:02:49 +0300

    adding back older master stracture at front-end folder level only to test build meteror process

--- front-end/build.sh
+++ front-end/build.sh
@@ -10,11 +10,11 @@
 
 SECONDS=0
 OS_TARGET_PATH=".."
-OS_TARGET_NAME=calipso-source--$(date +%Y-%m-%d-%s).tar.gz
+OS_TARGET_NAME=osdna-meteor-frontend-$(date +%Y-%m-%d-%s).tar.gz
 OS_NAME=${PWD##*/}
 
 meteor build $OS_TARGET_PATH/ --architecture=os.linux.x86_64
-#mv $OS_TARGET_PATH/$OS_NAME.tar.gz $OS_TARGET_PATH/$OS_TARGET_NAME
+mv $OS_TARGET_PATH/$OS_NAME.tar.gz $OS_TARGET_PATH/$OS_TARGET_NAME
 
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
