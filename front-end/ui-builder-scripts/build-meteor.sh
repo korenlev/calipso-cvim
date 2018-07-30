1b93a619b29944d05eebbf130345f7e85e4e46db
Koren Lev <korlev@cisco.com>
2018-07-30 17:47:08 +0300

    full workflow to create ui images

---
+++ front-end/ui-builder-scripts/build-meteor.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#
+# builds a production meteor bundle directory
+#
+set -e
+
+# set up npm auth token if one is provided
+if [[ "$NPM_TOKEN" ]]; then
+  echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+fi
+
+# Fix permissions warning in Meteor >=1.4.2.1 without breaking
+# earlier versions of Meteor with --unsafe-perm or --allow-superuser
+# https://github.com/meteor/meteor/issues/7959
+export METEOR_ALLOW_SUPERUSER=true
+
+cd $APP_SOURCE_DIR
+
+# Install app deps
+printf "\n[-] Running npm install in app directory...\n\n"
+meteor npm install
+
+# Removing buggy minifier package
+printf "\n[-] Removing buggy minifier...\n\n"
+meteor remove standard-minifier-js
+
+# build the bundle
+printf "\n[-] Building Meteor application...\n\n"
+mkdir -p $APP_BUNDLE_DIR
+meteor build --directory $APP_BUNDLE_DIR --server-only
+
+# run npm install in bundle
+printf "\n[-] Running npm install in the server bundle...\n\n"
+cd $APP_BUNDLE_DIR/bundle/programs/server/
+meteor npm install --production
+
+# fix fibers bug
+printf "\n[-] Fixing Fibers PATH bug in node>8.x...\n\n"
+cd $APP_BUNDLE_DIR/bundle/programs/server/
+npm remove fibers && npm install fibers
+
+# copy the entrypoint script in WORKDIR
+cp $BUILD_SCRIPTS_DIR/entrypoint.sh $APP_BUNDLE_DIR/bundle/entrypoint.sh
+
+# change ownership of the app to the node user
+chown -R node:node $APP_BUNDLE_DIR
