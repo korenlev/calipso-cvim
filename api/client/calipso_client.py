9b624526caea0e9d4d92fd9521487e23ced68746
korenlev <korenlev@gmail.com>
2019-05-07 14:37:12 +0300

    Added scan request and scan status handling for both one time and schedules

--- api/client/calipso_client.py
+++ api/client/calipso_client.py
@@ -2,12 +2,19 @@ import requests
 import urlparse
 import json
 import time
+import datetime
+# This is a calipso api client designed to be small and simple
+# currently for single pod, single api_server and single environment
+# assuming environment_config details are already deployed by CVIM automation
+# for central CVIM monitoring we'll need to add to this client per pod scanning
+# and allow adding multiple environments
 
 
 class CalipsoClient:
 
     def __init__(self):
         self.api_server = "korlev-calipso-testing"
+        self.client_version = "0.1.9"
         self.username = "calipso"
         self.password = "calipso_default"
         self.port = "8000"
@@ -15,6 +22,7 @@ class CalipsoClient:
         self.auth_url = self.base_url + "/auth/tokens"
         self.headers = {'Content-type': 'application/json'}
         self.token = None
+        self.time = None
         self.auth_body = {
             "auth": {
                 "methods": ["credentials"],
@@ -41,7 +49,6 @@ class CalipsoClient:
         url = urlparse.urljoin(self.base_url, endpoint)
         self.get_token()
         if method == 'post':
-            print payload
             response = requests.post(url, json=payload, headers=self.headers)
         elif method == 'delete':
             response = requests.delete(url, headers=self.headers)
@@ -52,40 +59,75 @@ class CalipsoClient:
         content = json.loads(response.content)
         return content
 
+    def scan_request(self, environment, scheduled=False, freq="DAILY"):
+        self.time = datetime.datetime.now().isoformat()
+        if scheduled is False:
+            request_payload = {
+                "log_level": "warning",
+                "clear": True,
+                "scan_only_inventory": False,
+                "scan_only_links": False,
+                "scan_only_cliques": False,
+                "environment": environment
+            }
+            return self.call_api('post', 'scans', request_payload)
+        else:
+            request_payload = {
+                "freq": freq,
+                "log_level": "warning",
+                "clear": True,
+                "scan_only_links": False,
+                "scan_only_cliques": False,
+                "environment": environment,
+                "scan_only_inventory": False,
+                "submit_timestamp": self.time
+            }
+            return self.call_api('post', 'scheduled_scans', request_payload)
+
+    def scan_status(self, environment, scheduled=False):
+        if scheduled is False:
+            params = {"env_name": environment}
+            reply = self.call_api('get', 'scans', params)
+            scans = reply["scans"]
+            for scan in scans:
+                print scan["status"], scan["id"]
+        else:
+            params = {"environment": environment}
+            reply = self.call_api('get', 'scheduled_scans', params)
+            schedules = reply["scheduled_scans"]
+            for schedule in schedules:
+                print schedule["scheduled_timestamp"], schedule["id"]
+
 
 cc = CalipsoClient()
-print cc.get_token()
-print cc.headers
-# get all environment_configs
+# print cc.get_token()
+# print cc.headers
+
+# get all environment_configs, with their names
 print cc.call_api('get', 'environment_configs')
-# get specific environment_config
+# get a specific environment_config
 print cc.call_api('get', 'environment_configs', payload={"name": "staging"})
-# post a scan request for a specific environment
-scan_request_payload = {
-    "log_level": "warning",
-    "clear": True,
-    "scan_only_inventory": False,
-    "scan_only_links": False,
-    "scan_only_cliques": False,
-    "environment": "staging"
-}
-scan_request_reply = cc.call_api('post', 'scans', scan_request_payload)
-print scan_request_reply
+
+# post a scan request, with defaults, for a specific environment
+print cc.scan_request(environment="staging", scheduled=False)
+# post a scheduled scan, with defaults, for a specific environment
+print cc.scan_request(environment="staging", scheduled=True, freq="WEEKLY")
+
 # then here we will place a check for specific scan id status
 # once VIMNET-1860 is implemented - to add scan 'id' into reply
-time.sleep(140)
+time.sleep(120)
 # get scans made for a specific environment (for status of scan etc)
-scan_params = {"env_name": "staging"}
-scan_reply = cc.call_api('get', 'scans', scan_params)
-scans = scan_reply["scans"]
-for scan in scans:
-    print scan["status"], scan["id"]
+cc.scan_status(environment="staging", scheduled=False)
+cc.scan_status(environment="staging", scheduled=True)
+
+
 # get network object from inventory
 inv_params = {"env_name": "staging",
               "id": "01776a49-a522-41ab-ab7c-94f4297c4227"}
 staging_network = cc.call_api('get', 'inventory', inv_params)
 print staging_network
 print "TYPE_DRIVER=", staging_network["provider:network_type"]
+
 # another inventory example for instances:
 inv_params = {"env_name": "staging",
               "type": "instance"}
