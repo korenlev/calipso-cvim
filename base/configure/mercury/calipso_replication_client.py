52a86b4e7b44bc9fc7b0ede5e6531a1505caab6a
korenlev <korenlev@gmail.com>
2019-05-12 19:37:31 +0300

    resolved some bson encoding for write to central ...

--- base/configure/mercury/calipso_replication_client.py
+++ base/configure/mercury/calipso_replication_client.py
@@ -3,6 +3,7 @@ import os
 import time
 import traceback
 from bson import json_util
+from bson import BSON
 try:
     from urllib import quote_plus
 except ImportError:
@@ -54,8 +55,8 @@ class MongoConnector(object):
     def insert(self, collection, docs):
         return self.database[collection].insert(docs)
 
-    def get_collection(self, db_name, collection):
-        file_name = "%s-%s.json" % (db_name, collection)
+    def get_collection(self, srv_name, collection):
+        file_name = "%s-%s.json" % (srv_name, collection)
         cursor = self.database[collection].find()
         f = open(os.path.join(BACKUP_DATA_PATH, file_name), "w")
         f.write('[')
@@ -71,17 +72,18 @@ class MongoConnector(object):
             elif qnt_cursor == num_max:
                 f.write(json.dumps(document, indent=4, sort_keys=False, default=json_util.default))
         f.write(']')
-        print ("exported data saved to {}/{}-{}.json".format(BACKUP_DATA_PATH, db_name, collection))
+        print ("exported data saved to {}/{}-{}.json".format(BACKUP_DATA_PATH, srv_name, collection))
         return f
 
-    def insert_collection(self, db_name, collection):
-        file_name = "%s-%s.json" % (db_name, collection)
+    def insert_collection(self, srv_name, collection):
+        file_name = "%s-%s.json" % (srv_name, collection)
         with open(os.path.join(BACKUP_DATA_PATH, file_name)) as f:
-            data = json.load(f)
+            j_data = json.load(f)
+            data = BSON.encode(j_data)
             self.remove_collection(collection)
             doc_ids = self.insert(collection, data)
             doc_count = len(doc_ids) if isinstance(doc_ids, list) else 1
-            print("Inserted '%s' collection in db. Documents inserted: %s" % (collection, doc_count))
+            print("Inserted '%s' collection in central db. Documents inserted: %s" % (collection, doc_count))
 
 
 def backoff(i):
@@ -89,37 +91,37 @@ def backoff(i):
 
 
 if __name__ == "__main__":
-    DBN = input("How many DBs to replicate? ")
-    DB = {}
-    DBs = []
+    SRVN = input("How many SRVs to replicate? ")
+    SRV = {}
+    SRVs = []
     n = 1
-    while n < DBN + 1:
+    while n < SRVN + 1:
         remote_name = raw_input("Remote Calipso Server {} Hostname/IP\n".format(n))
         remote_secret = raw_input("Remote Calipso Server {} Secret\n".format(n))
-        DB.update({"name": remote_name, "secret": remote_secret})
-        DBs.append(DB)
+        SRV.update({"name": remote_name, "secret": remote_secret})
+        SRVs.append(SRV)
         n += 1
     collection_names = ["inventory", "links", "messages", "scans", "scheduled_scans",
                         "statistics", "cliques"]
-    local_name = raw_input("Central Calipso Server Hostname/IP\n")
-    local_secret = raw_input("Central Calipso Server Secret\n")
+    central_name = raw_input("Central Calipso Server Hostname/IP\n")
+    central_secret = raw_input("Central Calipso Server Secret\n")
     attempt = 2
     while True:
         try:
             # read from remote DBs and export to local json files
-            for d in DBs:
-                mongo_connector = MongoConnector(d["name"], DEFAULT_PORT,
-                                                 DEFAULT_USER, d["secret"], AUTH_DB)
-                print ("Exporting collections from {}...".format(DB["name"]))
+            for s in SRVs:
+                mongo_connector = MongoConnector(s["name"], DEFAULT_PORT,
+                                                 DEFAULT_USER, s["secret"], AUTH_DB)
+                print ("Exporting collections from {}...".format(s["name"]))
                 for col in collection_names:
                     print("Exporting Collection {}...".format(col))
-                    mongo_connector.get_collection(d["name"], col)
+                    mongo_connector.get_collection(s["name"], col)
                     mongo_connector.disconnect()
                     # write from all local json files into the central DB
-                    mongo_connector = MongoConnector(local_name, DEFAULT_PORT,
-                                                     DEFAULT_USER, local_secret, AUTH_DB)
-                    print ("Importing data from {}-{}.json...".format(d["name"], col))
-                    mongo_connector.insert_collection(d["name"], col)
+                    mongo_connector = MongoConnector(central_name, DEFAULT_PORT,
+                                                     DEFAULT_USER, central_secret, AUTH_DB)
+                    print ("Importing data from {}-{}.json...".format(s["name"], col))
+                    mongo_connector.insert_collection(s["name"], col)
             break
         except:
             traceback.print_exc()
