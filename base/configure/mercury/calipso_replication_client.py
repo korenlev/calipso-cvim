07a3c1df0601f729a664c255f8e5a49716430d7e
korenlev <korenlev@gmail.com>
2019-05-12 19:12:46 +0300

    initial calipso_replication_client - still need to resolve bson encoding for write to central ...

---
+++ base/configure/mercury/calipso_replication_client.py
@@ -0,0 +1,133 @@
+import json
+import os
+import time
+import traceback
+from bson import json_util
+try:
+    from urllib import quote_plus
+except ImportError:
+    from urllib.parse import quote_plus
+from pymongo import MongoClient
+
+BACKUP_DATA_PATH = """C:\it_logs"""
+DEFAULT_PORT = 27017
+DEFAULT_USER = 'calipso'
+AUTH_DB = 'calipso'
+
+max_connection_attempts = 5
+
+
+class MongoConnector(object):
+    def __init__(self, host, port, user, pwd, db):
+        # Create calipso db and user if they don't exist
+        base_uri = "mongodb://%s:%s/" % (host, port)
+        base_client = MongoClient(base_uri)
+        base_client.close()
+
+        self.host = host
+        self.port = port
+        self.user = user
+        self.pwd = pwd
+        self.db = db
+
+        self.uri = None
+        self.client = None
+        self.database = None
+        self.connect()
+
+    def connect(self):
+        self.disconnect()
+        self.uri = "mongodb://%s:%s@%s:%s/%s" % (quote_plus(self.user), quote_plus(self.pwd),
+                                                 self.host, self.port, self.db)
+        self.client = MongoClient(self.uri)
+        self.database = self.client[self.db]
+
+    def disconnect(self):
+        if self.client:
+            print("Disconnecting from mongo...")
+            self.client.close()
+            self.client = None
+
+    def remove_collection(self, collection):
+        self.database[collection].remove()
+
+    def insert(self, collection, docs):
+        return self.database[collection].insert(docs)
+
+    def get_collection(self, db_name, collection):
+        file_name = "%s-%s.json" % (db_name, collection)
+        cursor = self.database[collection].find()
+        f = open(os.path.join(BACKUP_DATA_PATH, file_name), "w")
+        f.write('[')
+        qnt_cursor = 0
+        for document in cursor:
+            qnt_cursor += 1
+            num_max = cursor.count()
+            if num_max == 1:
+                f.write(json.dumps(document, indent=4, sort_keys=False, default=json_util.default))
+            elif num_max >= 1 and qnt_cursor <= num_max - 1:
+                f.write(json.dumps(document, indent=4, sort_keys=False, default=json_util.default))
+                f.write(',')
+            elif qnt_cursor == num_max:
+                f.write(json.dumps(document, indent=4, sort_keys=False, default=json_util.default))
+        f.write(']')
+        print ("exported data saved to {}/{}-{}.json".format(BACKUP_DATA_PATH, db_name, collection))
+        return f
+
+    def insert_collection(self, db_name, collection):
+        file_name = "%s-%s.json" % (db_name, collection)
+        with open(os.path.join(BACKUP_DATA_PATH, file_name)) as f:
+            data = json.load(f)
+            self.remove_collection(collection)
+            doc_ids = self.insert(collection, data)
+            doc_count = len(doc_ids) if isinstance(doc_ids, list) else 1
+            print("Inserted '%s' collection in db. Documents inserted: %s" % (collection, doc_count))
+
+
+def backoff(i):
+    return 2 ** (i - 1)
+
+
+if __name__ == "__main__":
+    DBN = input("How many DBs to replicate? ")
+    DB = {}
+    DBs = []
+    n = 1
+    while n < DBN + 1:
+        remote_name = raw_input("Remote Calipso Server {} Hostname/IP\n".format(n))
+        remote_secret = raw_input("Remote Calipso Server {} Secret\n".format(n))
+        DB.update({"name": remote_name, "secret": remote_secret})
+        DBs.append(DB)
+        n += 1
+    collection_names = ["inventory", "links", "messages", "scans", "scheduled_scans",
+                        "statistics", "cliques"]
+    local_name = raw_input("Central Calipso Server Hostname/IP\n")
+    local_secret = raw_input("Central Calipso Server Secret\n")
+    attempt = 2
+    while True:
+        try:
+            # read from remote DBs and export to local json files
+            for d in DBs:
+                mongo_connector = MongoConnector(d["name"], DEFAULT_PORT,
+                                                 DEFAULT_USER, d["secret"], AUTH_DB)
+                print ("Exporting collections from {}...".format(DB["name"]))
+                for col in collection_names:
+                    print("Exporting Collection {}...".format(col))
+                    mongo_connector.get_collection(d["name"], col)
+                    mongo_connector.disconnect()
+                    # write from all local json files into the central DB
+                    mongo_connector = MongoConnector(local_name, DEFAULT_PORT,
+                                                     DEFAULT_USER, local_secret, AUTH_DB)
+                    print ("Importing data from {}-{}.json...".format(d["name"], col))
+                    mongo_connector.insert_collection(d["name"], col)
+            break
+        except:
+            traceback.print_exc()
+            if attempt >= max_connection_attempts:
+                raise ValueError("Failed to connect to mongodb. Tried %s times" % attempt)
+            attempt += 1
+            print("Waiting for mongodb to come online... Attempt #%s" % attempt)
+            time.sleep(backoff(attempt))
+        mongo_connector.disconnect()
+        print("Workload completed")
+        exit(0)
