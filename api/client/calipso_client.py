95603695fd96404fbf9e52e21c6c796f5f42a14e
korenlev <korenlev@gmail.com>
2019-05-14 18:02:12 +0300

    client is now ready to be released - 0.2.0 tag

--- api/client/calipso_client.py
+++ api/client/calipso_client.py
@@ -183,7 +183,7 @@ def run():
                         help="get a reply back with calipso_client version",
                         action='version',
                         default=None,
-                        version='%(prog)s version: 0.1.19')
+                        version='%(prog)s version: 0.2.0')
 
     args = parser.parse_args()
 
