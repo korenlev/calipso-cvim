a6b80f9a0a6cacf363a874a7fb67e396464df6c7
korenlev <korenlev@gmail.com>
2019-05-23 11:13:31 -0700

    move bin and py clients to ver 0.2.6
    Change-Id: I47a0b8a9d5c26258c25d41111c710ed331d0fe74

--- api/client/calipso_client.py
+++ api/client/calipso_client.py
@@ -187,7 +187,7 @@ def run():
                         help="get a reply back with calipso_client version",
                         action='version',
                         default=None,
-                        version='%(prog)s version: 0.2.5')
+                        version='%(prog)s version: 0.2.6')
 
     args = parser.parse_args()
 
