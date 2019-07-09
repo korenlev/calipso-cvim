6da1899864750e6467a4e2a962200e0e48c88bc4
korenlev <korenlev@gmail.com>
2019-07-09 20:28:37 +0300

    debug script to add rpms if missing, for engineering devops

--- api/client/cvim_remote_debug.sh
+++ api/client/cvim_remote_debug.sh
@@ -6,6 +6,11 @@ api_container=$(docker ps | awk '{print $NF}' | grep calipso_api)
 if [[ "$1" = "enable" ]]; then
     docker exec -i ${scan_container} sed -ie '/^export/a nohup /usr/sbin/sshd -D &' /start_calipso_scan_manager.sh
     docker exec -i ${scan_container} sed -ie '/^#AddressFamily/a Port 30022' /etc/ssh/sshd_config
+    docker exec -i ${scan_container} wget http://mirror.centos.org/centos/7/os/x86_64/Packages/tcp_wrappers-libs-7.6-77.el7.x86_64.rpm
+    docker exec -i ${scan_container} wget http://mirror.centos.org/centos/7/os/x86_64/Packages/openssh-server-7.4p1-16.el7.x86_64.rpm
+    docker exec -i ${scan_container} rpm -i tcp_wrappers-libs-7.6-77.el7.x86_64.rpm
+    docker exec -i ${scan_container} rpm -i openssh-server-7.4p1-16.el7.x86_64.rpm
+    docker exec -i ${scan_container} ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
     echo -e "calipso_debug" | docker exec -i ${scan_container} passwd --stdin root
     echo 'source scl_source enable rh-python35' | docker exec -i ${scan_container} tee --append /root/.bash_profile
     echo 'source /var/lib/calipso/calipso_config' | docker exec -i ${scan_container} tee --append /root/.bash_profile
