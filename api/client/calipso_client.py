743f3a8c3902491a0bbe94c446408056a9aa0068
korenlev <korenlev@gmail.com>
2019-07-09 23:08:18 +0300

    latest version bump for clients with latest changes
    Change-Id: I647af76bbe62fb9feeebf1c7c68d7f0c5e5b9659

--- api/client/calipso_client.py
+++ api/client/calipso_client.py
@@ -199,7 +199,7 @@ def run():
                         help="get a reply back with calipso_client version",
                         action='version',
                         default=None,
-                        version='%(prog)s version: 0.3.1')
+                        version='%(prog)s version: 0.3.2')
 
     args = parser.parse_args()
 
--- api/client/calipso_replication_client.py
+++ api/client/calipso_replication_client.py
@@ -170,7 +170,7 @@ def run():
                         help="get a reply back with replication_client version",
                         action='version',
                         default=None,
-                        version='%(prog)s version: 0.3.1')
+                        version='%(prog)s version: 0.3.2')
 
     args = parser.parse_args()
 
