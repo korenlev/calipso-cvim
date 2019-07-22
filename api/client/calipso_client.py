2b5bc60cd2470889a13cde7c6495c37eccded9ca
korenlev <korenlev@gmail.com>
2019-07-22 20:52:14 +0300

    bump to 0.3.7 after regression bug fix (k8s scan)

--- api/client/calipso_client.py
+++ api/client/calipso_client.py
@@ -199,7 +199,7 @@ def run():
                         help="get a reply back with calipso_client version",
                         action='version',
                         default=None,
-                        version='%(prog)s version: 0.3.6')
+                        version='%(prog)s version: 0.3.7')
 
     args = parser.parse_args()
 
--- api/client/calipso_replication_client.py
+++ api/client/calipso_replication_client.py
@@ -168,7 +168,7 @@ def run():
                         help="get a reply back with replication_client version",
                         action='version',
                         default=None,
-                        version='%(prog)s version: 0.3.6')
+                        version='%(prog)s version: 0.3.7')
 
     args = parser.parse_args()
 
