554d44b061f1ddafa75803f6c3b7b3c17eb714f2
korenlev <korenlev@gmail.com>
2019-07-23 13:33:59 +0300

    move slower to help docker daemon CLI handle requests
    Change-Id: I40c073ce09b6d76981214f9295d1c60b116fd065

--- base/install/calipso_update.sh
+++ base/install/calipso_update.sh
@@ -14,6 +14,7 @@ git pull
 
 # commit code changes to new images
 echo "commiting code changes..."
+sleep 1
 docker commit calipso-test korenlev/calipso:test-v2
 docker commit calipso-monitor korenlev/calipso:monitor-v2
 docker commit calipso-scan korenlev/calipso:scan-v2
@@ -22,6 +23,7 @@ docker commit calipso-listen korenlev/calipso:listen-v2
 
 # push new images to dockerhub
 echo "pushing to dockerhub..."
+sleep 1
 docker push korenlev/calipso:test-v2
 docker push korenlev/calipso:monitor-v2
 docker push korenlev/calipso:scan-v2
@@ -30,10 +32,12 @@ docker push korenlev/calipso:listen-v2
 
 # kill and remove running containers using new calipso-installer
 echo "killing running containers..."
+sleep 1
 python3 /home/calipso/Calipso/base/install/calipso-installer.py --command stop-all
 
 # remove new local images
 echo "removing new local images..."
+sleep 1
 docker rmi -f korenlev/calipso:test-v2
 docker rmi -f korenlev/calipso:monitor-v2
 docker rmi -f korenlev/calipso:scan-v2
@@ -42,6 +46,7 @@ docker rmi -f korenlev/calipso:listen-v2
 
 # pull for new images from dockerhub
 echo "pulling new remote images from dockerhub..."
+sleep 1
 docker pull korenlev/calipso:test-v2
 docker pull korenlev/calipso:monitor-v2
 docker pull korenlev/calipso:scan-v2
@@ -50,10 +55,12 @@ docker pull korenlev/calipso:listen-v2
 
 # remove old local untagged images
 echo "removing old local images..."
+sleep 5
 docker rmi -f $(sudo docker images --filter 'dangling=true' -q --no-trunc)
 
 # tag new images for cisco docker repo
 echo "tagging for cisco docker repo..."
+sleep 1
 docker tag korenlev/calipso:test-v2 cloud-docker.cisco.com/calipso:test-v2
 docker tag korenlev/calipso:monitor-v2 cloud-docker.cisco.com/calipso:monitor-v2
 docker tag korenlev/calipso:scan-v2 cloud-docker.cisco.com/calipso:scan-v2
@@ -62,6 +69,7 @@ docker tag korenlev/calipso:listen-v2 cloud-docker.cisco.com/calipso:listen-v2
 
 # push new images to cisco docker repo
 echo "pushing to cisco docker repo..."
+sleep 1
 docker push cloud-docker.cisco.com/calipso:test-v2
 docker push cloud-docker.cisco.com/calipso:monitor-v2
 docker push cloud-docker.cisco.com/calipso:scan-v2
