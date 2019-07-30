027bbeb74f494dcf261ebb6c69931354547b3cc4
korenlev <korenlev@gmail.com>
2019-07-30 16:04:19 +0300

    bump clients to 0.4.0 , new tag for 3.4.0 release

--- api/client/calipso_replication_client.py
+++ api/client/calipso_replication_client.py
@@ -180,7 +180,7 @@ def run():
                         help="get a reply back with replication_client version",
                         action='version',
                         default=None,
-                        version='%(prog)s version: 0.3.13')
+                        version='%(prog)s version: 0.4.0')
 
     args = parser.parse_args()
 
