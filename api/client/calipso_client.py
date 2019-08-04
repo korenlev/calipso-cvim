102ad70ab00b05f48e3dce57d0905befb92c22dd
korenlev <korenlev@gmail.com>
2019-08-04 20:55:45 +0300

    support latest VPP version for monitoring check

--- api/client/calipso_client.py
+++ api/client/calipso_client.py
@@ -215,7 +215,7 @@ def run():
                         help="get a reply back with calipso_client version",
                         action='version',
                         default=None,
-                        version='%(prog)s version: 0.4.1')
+                        version='%(prog)s version: 0.4.3')
 
     args = parser.parse_args()
 
