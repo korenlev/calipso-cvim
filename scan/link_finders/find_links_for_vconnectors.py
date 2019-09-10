d9e2ba555225f200f0a549fc00eb2aedea3ad9d3
Ilia Abashin <iabashin@cisco.com>
2019-09-10 16:00:26 +0300

    K8s fixes

--- scan/link_finders/find_links_for_vconnectors.py
+++ scan/link_finders/find_links_for_vconnectors.py
@@ -71,9 +71,8 @@ class FindLinksForVconnectors(FindLinks):
         if not vnic:
             return
         link_name = vnic["mac_address"]
-        attributes = {
-            'vedge_type': vnic['vedge_type']
-        }
+        attributes = {'vedge_type': vnic['vedge_type']} if 'vedge_type' in vnic else {}
+
         if 'network' in vnic:
             attributes['network'] = vnic['network']
             vconnector['network'] = vnic['network']
@@ -109,5 +108,6 @@ class FindLinksForVconnectors(FindLinks):
         })
         if not vedge:
             return
-        self.link_items(vconnector, vedge,
-                        link_name=vedge['name'], extra_attributes={'vedge_type': vedge['vedge_type']})
+
+        attributes = {'vedge_type': vedge['vedge_type']} if 'vedge_type' in vedge else {}
+        self.link_items(vconnector, vedge, link_name=vedge['name'], extra_attributes=attributes)
