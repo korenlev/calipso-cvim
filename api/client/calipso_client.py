df33ae449d89a2318f5d2cb3bafc95f6aa71a7ee
Koren Lev <korenlev@gmail.com>
2019-09-11 18:52:58 +0300

    let clients match latest tag
    Change-Id: Iad5415a9e18d3736307efe89ede188637f9705ea

--- api/client/calipso_client.py
+++ api/client/calipso_client.py
@@ -246,7 +246,7 @@ def run():
                         help="get a reply back with calipso_client version",
                         action='version',
                         default=None,
-                        version='%(prog)s version: 0.4.14')
+                        version='%(prog)s version: 0.4.15')
 
     args = parser.parse_args()
 
