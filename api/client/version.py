a98435235f1939c74d4cf3771dc64a5a7a40f5c8
korenlev <korenlev@gmail.com>
2020-04-29 02:35:31 +0300

    bug fix for aio controllers that are also ceph storage hosts , bump 0.9.6

--- api/client/version.py
+++ api/client/version.py
@@ -7,4 +7,4 @@
 # which accompanies this distribution, and is available at                    #
 # http://www.apache.org/licenses/LICENSE-2.0                                  #
 ###############################################################################
-VERSION = "0.9.5"
+VERSION = "0.9.6"
